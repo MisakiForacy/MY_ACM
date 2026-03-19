9a2c169d3b2e5b20d40774283e1f7319a76ba0a2
MisakiForacy <fuxuanyu243@gmail.com>
2026-03-20 00:30:00 +0800

    Auto sync 2026/03/20 周五  0:30:00.84

--- auto_sync/fix_task.ps1
+++
@@ -1,7 +0,0 @@
-$a = New-ScheduledTaskAction -Execute "D:\MY_CODE\ACM\auto_sync\auto_sync.bat"
-$t = New-ScheduledTaskTrigger -Daily -At "00:00"
-$s = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
-Unregister-ScheduledTask -TaskName "ACM_Auto_Sync" -Confirm:$false -ErrorAction SilentlyContinue
-Register-ScheduledTask -TaskName "ACM_Auto_Sync" -Action $a -Trigger $t -Settings $s -Force
-Write-Host "Task created. Current path: D:\MY_CODE\ACM\auto_sync\auto_sync.bat"
-Write-Host "To add 30-minute interval, use: schtasks /change /tn ACM_Auto_Sync /ri 30"
