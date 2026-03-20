0d4690b2f9af7d03fb7090015a7ce706350742e0
MisakiForacy <fuxuanyu243@gmail.com>
2026-03-20 18:30:01 +0800

    Auto sync 2026/03/20 周五 18:30:01.27

---
+++ HDOJ/2026-3-20/1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define siz(x) ((int)x.size())
+#define all(x) begin(x),end(x)
+
+#define x first 
+#define y second 
+
+using namespace std;
+using LL = long long;
+using PLL = pair<LL, LL>;
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T --) solve();
+}
