da9dc4fadd809a25c9cf3cee0b368df7b264d93b
MisakiForacy <fuxuanyu243@gmail.com>
2026-03-20 16:30:00 +0800

    Auto sync 2026/03/20 周五 16:30:00.91

--- HDOJ/2026-3-13/5.cpp
+++ HDOJ/2026-3-13/5.cpp
@@ -15,12 +15,13 @@ void solve() {
     cin >> n >> k >> m;
     vector<LL> a(n + 1), p(n + 1, 0);
     vector<vector<LL>> dp(n + 1, vector<LL> (k + 2, 0));
-    // dp[i][j] 表示
+    // dp[i][j] 表示取到第i位，包括自己，后面有j个a[i] / 2
+    // dp[i][j] = dp[i - m - 1][]
     for (int i = 1;i <= n;i ++) cin >> a[i], p[i] = p[i - 1] + a[i];
     for (int i = 1;i <= n;i ++) {
         for (int j = 0;j <= k + 1;j ++) {
             if (j == 0) {
-                for 
+                for (int )
             } else if (j <= k) {
 
             } else {
