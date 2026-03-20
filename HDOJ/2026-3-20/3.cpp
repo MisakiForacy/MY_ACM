916c2552149b9dce5c062b09a8d739ac13dc3ec6
MisakiForacy <fuxuanyu243@gmail.com>
2026-03-20 22:00:01 +0800

    Auto sync 2026/03/20 周五 22:00:01.37

--- HDOJ/2026-3-20/3.cpp
+++ HDOJ/2026-3-20/3.cpp
@@ -10,11 +10,74 @@ using namespace std;
 using LL = long long;
 using PLL = pair<LL, LL>;
 
+typedef pair<int, int> pii;
+
+const int mod1 = 1e9 + 7;
+const int mod2 = 1e9 + 9;
+vector<pii> pw;
+pii base;
+mt19937 rnd(time(0));
+
+pii operator+(const pii& a, const pii& b) {
+    int c1 = a.fi + b.fi, c2 = a.se + b.se;
+    if (c1 >= mod1) c1 -= mod1;
+    if (c2 >= mod2) c2 -= mod2;
+    return { c1, c2 };
+}
+
+pii operator-(const pii& a, const pii& b) {
+    int c1 = a.fi - b.fi, c2 = a.se - b.se;
+    if (c1 < 0) c1 += mod1;
+    if (c2 < 0) c2 += mod2;
+    return { c1, c2 };
+}
+
+pii operator*(const pii& a, const pii& b) {
+    return { 1LL * a.fi * b.fi % mod1, 1LL * a.se * b.se % mod2 };
+}
+
+void init_strhash(int lim = 0) {
+    pw = vector<pii>(lim + 1);
+    base = { rnd() % mod1, rnd() % mod2 };
+    pw[0] = { 1, 1 };
+    for (int i = 1; i <= lim; i++) pw[i] = pw[i - 1] * base;
+}
+
+struct Str_hash {
+    vector<pii> v;
+
+    Str_hash() {}
+
+    void init(const string& s) {
+        int m = s.size();
+        v.resize(m + 1);
+        for (int j = 1; j <= m; j++) {
+            char ch = s[j - 1];
+            v[j] = v[j - 1] * base + make_pair(ch, ch);
+        }
+    }
+
+    pii get(int L, int R) {
+        return v[R] - (v[L - 1] * pw[R - L + 1]);
+    }
+};
+
 void solve() {
     string s, t;
     cin >> s >> t;
-    reverse(all(t));
-    
+    int n = siz(s);
+    init_strhash(n);
+    Str_hash hs, ht;
+    hs.init(s), ht.init(t);
+    for (int i = 1;i <= n;i ++) {
+        for (int j = i;j <= n;j ++) {
+            LL hash = hs.get(i, j);
+            int len = j - i + 1;
+            for (int l = 1;l + len - 1 <= n;l ++) {
+                
+            }
+        }
+    }
 }
 
 int main() {
