2b717fcc824787a2260c1a5697cb5f86cfacdc29
MisakiForacy <fuxuanyu243@gmail.com>
2026-03-20 21:30:01 +0800

    Auto sync 2026/03/20 周五 21:30:01.38

--- HDOJ/2026-3-20/5.cpp
+++ HDOJ/2026-3-20/5.cpp
@@ -11,28 +11,9 @@ using LL = long long;
 using PLL = pair<LL, LL>;
 
 void solve() {
-    LL n, k;
-    cin >> n >> k;
-    if (k >= 60) {
-        cout << 1 << '\n';
-    } else {
-        auto check = [&](LL x) -> bool {
-            LL res = 1;
-            for (int i = 1;i <= k;i ++) {
-                res *= x;
-                if (res > n) return 0;
-            }
-            return res <= n;
-        };
-        LL lo = 1, hi = 100000;
-        while (lo < hi) {
-            LL mid = lo + hi >> 1;
-            if (!check(mid)) hi = mid;
-            else lo = mid + 1;
-        }
-        if (!check(lo)) lo --;
-        cout << lo << '\n';
-    }
+    long double N, K;
+    cin >> N >> K;
+    cout << floor(pow(N,1.0/K)) << '\n';
 }
 
 int main() {
