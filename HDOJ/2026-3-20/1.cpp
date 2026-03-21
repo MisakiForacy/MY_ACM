bdff000974377e370ea39b78f658d4503de71efc
MisakiForacy <fuxuanyu243@gmail.com>
2026-03-21 10:00:01 +0800

    Auto sync 2026/03/21 周六 10:00:01.87

--- HDOJ/2026-3-20/1.cpp
+++ HDOJ/2026-3-20/1.cpp
@@ -1,22 +1,19 @@
 #include <bits/stdc++.h>
 
-#define siz(x) ((int)x.size())
-#define all(x) begin(x),end(x)
-
-#define x first 
-#define y second 
-
 using namespace std;
 using LL = long long;
-using PLL = pair<LL, LL>;
-
-void solve() {
-    
-}
 
 int main() {
-    ios::sync_with_stdio(0), cin.tie(0);
-    int T = 1;
-    cin >> T;
-    while (T --) solve();
+    int n;
+    cin >> n;
+    vector<vector<int>> f(n + 1, vector<int> (4, 0));
+    LL ans = 0;
+    f[1][1] = 1;
+    for (int i = 2;i <= n;i ++) {
+        f[i][1] = f[i - 1][2] + f[i - 1][3];
+        f[i][2] = f[i - 1][1] + f[i - 1][3];
+        f[i][3] = f[i - 1][2] + f[i - 1][1];
+    }
+    ans += f[n][2] + f[n][3];
+    cout << ans * 3 << '\n';
 }
