5935540b1ea62d3cc1574ebfeb3a0f25a6e85ce2
MisakiForacy <fuxuanyu243@gmail.com>
2026-03-23 10:40:03 +0800

    Auto sync 2026/03/23 周一 10:40:03.78

--- HDOJ/2026-3-20/3.cpp
+++ HDOJ/2026-3-20/3.cpp
@@ -12,21 +12,41 @@ using LL = long long;
 void solve() {
     string s, t;
     cin >> s >> t;
+    int n = siz(s);
     reverse(all(t));
     s = ' ' + s;
     t = ' ' + t;
-    vector<int> r1(n + 1, 1), r2(n + 1, 1);
+    vector<LL> p1(n + 1, 1), p2(n + 1, 0), p3(n + 1, 1), p4(n + 1, 0);
+    vector<LL> ps(n + 2, 1), pt(n + 2, 1);
+    ps[n + 1] = pt[n + 1] = 0;
     for (int i = 1;i <= n;i ++) {
-        while (i - r1[i] >= 1 && i + r1[i] <= n && s[i - r1[i]] == s[i + r1[i]]) r1[i] ++;
-        while (i - r2[i] >= 1 && i + r2[i] <= n && s[i - r2[i]] == s[i + r2[i]]) r2[i] ++;
+        while (i - p1[i] >= 1 && i + p1[i] <= n && s[i - p1[i]] == s[i + p1[i]]) {
+            ps[i - p1[i]] = max(ps[i - p1[i]], p1[i] * 2 + 1);
+            p1[i] ++;
+        }
+        while (i - p2[i] >= 2 && i + p2[i] <= n && s[i - p2[i] - 1] == s[i + p2[i]]) {
+            ps[i - p2[i] - 1] = max(ps[i - p2[i] - 1], (p2[i] + 1) * 2);
+            p2[i] ++;
+        }
+        while (i - p3[i] >= 1 && i + p3[i] <= n && s[i - p3[i]] == s[i + p3[i]]) {
+            pt[i - p3[i]] = max(pt[i - p3[i]], p3[i] * 2 + 1);
+            p3[i] ++;
+        }
+        while (i - p4[i] >= 2 && i + p4[i] <= n && s[i - p4[i] - 1] == s[i + p4[i]]) {
+            pt[i - p4[i] - 1] = max(pt[i - p4[i] - 1], (p4[i] + 1) * 2);
+            p4[i] ++;
+        }
     }
-    vector<vector<int>> f(n + 1, vector<int> (n + 1, 0));
+    vector<vector<LL>> f(n + 1, vector<LL> (n + 1, 0));
     // f[i][j] 表示s[1...i]和t[1...j]的最长公共子后缀长度
+    LL ans = 0;
     for (int i = 1;i <= n;i ++) {
         for (int j = 1;j <= n;j ++) {
             f[i][j] = f[i - 1][j - 1] + (s[i] == t[j]);
+            ans += f[i][j] * (ps[i + 1] + pt[j + 1] + 1);
         }
     }
+    cout << ans << '\n';
 }
 
 int main() {
--- 笔记/字符串/Manacher/Manacher.cpp
+++ 笔记/字符串/Manacher/Manacher.cpp
@@ -8,44 +8,44 @@ using LL = long long;
     马拉车算法：找字符串中最长回文串
 */
 
-string Switch(string s){
-    /*
-        将字符串s转换
-        原s为abbcac,
-        转换后结果形如$#a#b#b#c#a#c#^
-        原因
-        该算法原本只能处理长度为奇数的回文串
-        如此转换后可以同时处理奇数串和偶数串
-        让所有回文串长度都为奇数
-    */
-    string t = "&";
-    for (char &c : s){
-        t += "#";
-        t += c;
-    }
-    t += "#^";
-    return t;
-}
+// string Switch(string s){
+//     /*
+//         将字符串s转换
+//         原s为abbcac,
+//         转换后结果形如$#a#b#b#c#a#c#^
+//         原因
+//         该算法原本只能处理长度为奇数的回文串
+//         如此转换后可以同时处理奇数串和偶数串
+//         让所有回文串长度都为奇数
+//     */
+//     string t = "&";
+//     for (char &c : s){
+//         t += "#";
+//         t += c;
+//     }
+//     t += "#^";
+//     return t;
+// }
 
-int Manacher(string s){
-    string t = Switch(s);
-    vector<int> p(siz(t), 0);
-    int mr = 0, mid;
-    for (int i = 1;i < siz(t);i ++){
-        if (i < mr) p[i] = min(p[2 * mid - i], mr - i);
-        else p[i] = 1;
-        while (t[i - p[i]] == t[i + p[i]]) p[i] ++;
-        if (i + p[i] > mr){
-            mr = i + p[i];
-            mid = i;
-        }
-    }
-    int len = 0;
-    for (int i = 1;i < siz(t);i ++){
-        len = max(len, p[i] - 1);
-    }
-    return len;
-}
+// int Manacher(string s){
+//     string t = Switch(s);
+//     vector<int> p(siz(t), 0);
+//     int mr = 0, mid;
+//     for (int i = 1;i < siz(t);i ++){
+//         if (i < mr) p[i] = min(p[2 * mid - i], mr - i);
+//         else p[i] = 1;
+//         while (t[i - p[i]] == t[i + p[i]]) p[i] ++;
+//         if (i + p[i] > mr){
+//             mr = i + p[i];
+//             mid = i;
+//         }
+//     }
+//     int len = 0;
+//     for (int i = 1;i < siz(t);i ++){
+//         len = max(len, p[i] - 1);
+//     }
+//     return len;
+// }
 
 /*
 =============以上是ACWing的教程，以下是自己的优化写法，可以节省一倍空间================
@@ -57,6 +57,7 @@ int Manacher(string s){
 // abcba
 // 113
 
+// 我们定义p[i]​表示以字符s[i]​为中心的最长回文子串的半径
 int Manacher(string s){
     s = ' ' + s;
     vector<int> p(siz(s), 0);
@@ -71,6 +72,10 @@ int Manacher(string s){
         }
         Max = max(Max, p[i] * 2 - 1);
     }
+    for (int i = 1;i < siz(s);i ++) {
+        cout << p[i] << ' ';
+    }
+    cout << '\n';
     R = 0;
     for (int i = 1;i < siz(s) - 1;i ++) {
         if (s[i] != s[i + 1]) continue;
