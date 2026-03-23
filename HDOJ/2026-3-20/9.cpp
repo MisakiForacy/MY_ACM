fbd30cf94b39ded78bb64d14a194497736926b8c
MisakiForacy <fuxuanyu243@gmail.com>
2026-03-23 08:31:02 +0800

    Auto sync 2026/03/23 周一  8:31:02.26

--- HDOJ/2026-3-20/9.cpp
+++ HDOJ/2026-3-20/9.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 using LL = long long;
 
-const int off = 250;
+const int off = 100;
 
 struct node
 {
@@ -18,18 +18,18 @@ void solve() {
     queue<node> p;
     p.push({0, 0, 0});
     LL mi = 1e18;
-    vector<vector<int>> vis(500, vector<int> (500, 0));
+    vector<vector<int>> vis(201, vector<int> (201, 0));
     while (p.size()) {
         auto [xx, yy, dd] = p.front();
         p.pop();
         if (xx == 0 && yy == 0 && dd != 0) {
             mi = min(mi, dd);
         }
-        if (dd > 200) continue;
+        if (dd > 40000) continue;
         for (int i = 0;i < n;i ++) {
             int nx = xx + x[i];
             int ny = yy + y[i];
-            if (nx < -200 || nx > 200 || ny < -200 || ny > 200 || vis[nx + off][ny + off]) continue;
+            if (nx < -100 || nx > 100 || ny < -100 || ny > 100 || vis[nx + off][ny + off]) continue;
             p.push({nx, ny, dd + 1});
             vis[nx + off][ny + off] = 1;
         }
