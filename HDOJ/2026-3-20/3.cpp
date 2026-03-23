e4a3baed4f3b2ff9e49375a03bd2c5fea0d50a19
MisakiForacy <fuxuanyu243@gmail.com>
2026-03-23 13:30:28 +0800

    Auto sync 2026/03/23 周一 13:30:28.50

--- HDOJ/2026-3-20/3.cpp
+++ HDOJ/2026-3-20/3.cpp
@@ -17,23 +17,23 @@ void solve() {
     s = ' ' + s;
     t = ' ' + t;
     vector<LL> p1(n + 1, 1), p2(n + 1, 0), p3(n + 1, 1), p4(n + 1, 0);
-    vector<LL> ps(n + 2, 0), pt(n + 2, 0);
+    vector<LL> ps(n + 2, 1), pt(n + 2, 1);
     ps[n + 1] = pt[n + 1] = 0;
     for (int i = 1;i <= n;i ++) {
         while (i - p1[i] >= 1 && i + p1[i] <= n && s[i - p1[i]] == s[i + p1[i]]) {
-            ps[i - p1[i]] = max(ps[i - p1[i]], p1[i] * 2 - 1);
+            ps[i - p1[i]] = max(ps[i - p1[i]], p1[i] + 1);
             p1[i] ++;
         }
         while (i - p2[i] >= 2 && i + p2[i] <= n && s[i - p2[i] - 1] == s[i + p2[i]]) {
-            ps[i - p2[i] - 1] = max(ps[i - p2[i] - 1], p2[i] * 2);
+            ps[i - p2[i] - 1] = max(ps[i - p2[i] - 1], p2[i] + 1);
             p2[i] ++;
         }
-        while (i - p3[i] >= 1 && i + p3[i] <= n && s[i - p3[i]] == s[i + p3[i]]) {
-            pt[i - p3[i]] = max(pt[i - p3[i]], p3[i] * 2 - 1);
+        while (i - p3[i] >= 1 && i + p3[i] <= n && t[i - p3[i]] == t[i + p3[i]]) {
+            pt[i - p3[i]] = max(pt[i - p3[i]], p3[i] + 1);
             p3[i] ++;
         }
-        while (i - p4[i] >= 2 && i + p4[i] <= n && s[i - p4[i] - 1] == s[i + p4[i]]) {
-            pt[i - p4[i] - 1] = max(pt[i - p4[i] - 1], p4[i] * 2);
+        while (i - p4[i] >= 2 && i + p4[i] <= n && t[i - p4[i] - 1] == t[i + p4[i]]) {
+            pt[i - p4[i] - 1] = max(pt[i - p4[i] - 1], p4[i] + 1);
             p4[i] ++;
         }
     }
@@ -46,7 +46,7 @@ void solve() {
                 f[i][j] = f[i - 1][j - 1] + 1;
             else
                 f[i][j] = 0;
-            ans += f[i][j] * (ps[i] + pt[j] + 1);
+            ans += f[i][j] * (ps[i + 1] + pt[j + 1] + 1);
         }
     }
     cout << ans << '\n';
