c6d870a70169078f4d36d668ab70cc4c6bc3b551
MisakiForacy <fuxuanyu243@gmail.com>
2026-03-25 14:40:06 +0800

    Auto sync 2026/03/25 周三 14:40:06.72

--- AKICPC/2025 United Kingdom and Ireland Programming Contest (UKIEPC 2025)/G.cpp
+++ AKICPC/2025 United Kingdom and Ireland Programming Contest (UKIEPC 2025)/G.cpp
@@ -8,8 +8,8 @@ void solve() {
     cin >> n >> a >> b >> x >> y;
     LL v1 = 0, v2 = 0;
     v1 = min(n, x) * a + (n - min(n, x)) * b;
-    v2 = n / (x + y) * a * x + min(n % (x + y), x) * a;
-    if (n % (x + y) > x) v2 += (n % (x + y) - x) * b;
+    LL t = (n - x) / (y + x);
+    v2 = (t + 1) * x * a + max((n - t * (y + x) - x - y) * a, (n - t * (y + x) - x) * b);
     // cout << v1 << ' ' << v2 << '\n';
     cout << max(v1, v2) << '\n';
 }
