cff5dc82b5d0e315466345b3fa6ec4c1a4a6310a
MisakiForacy <fuxuanyu243@gmail.com>
2026-03-27 23:10:40 +0800

    Auto sync 2026/03/27 周五 23:10:40.23

--- HDOJ/2026-3-27/10.cpp
+++ HDOJ/2026-3-27/10.cpp
@@ -17,39 +17,13 @@ void init() {
     for (int i = 1;i <= 63;i ++) p2[i] = p2[i - 1] * 2;
 }
 
-/*
-0
-1 0 0
-1 1 0
-1 1 1 
-1 2
-2 0
-2 1
-3
-*/
-
-// 2^3 8
-
 void solve() {
     LL x, k;
     cin >> x >> k;
     vector<LL> a(x + 1);
     int p = 1;
     for (int i = x;i >= 1;i --) {
-        if (!k) break;
-        if (i > 60 || k <= p2[i - 1]) {
-            a[p ++] = 1;
-            k -= 1;
-        } else {
-            int t = 1;
-            while (k > p2[i - t]) {
-                k -= p2[i - t];
-                t ++;
-            }
-            k --;
-            a[p ++] = t;
-            i = i - t + 1;
-        }
+        
     }
     for (int i = 1;i < p;i ++) cout << a[i] << ' ';
     cout << '\n';
