3bdf2d877778aefc0da7ebfe1303b09b2dd2120b
MisakiForacy <fuxuanyu243@gmail.com>
2026-03-27 19:39:55 +0800

    Auto sync 2026/03/27 周五 19:39:55.34

---
+++ HDOJ/2026-3-27/4.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define x first 
+#define y second 
+
+#define siz(x) ((int)x.size())
+#define all(x) begin(x),end(x)
+
+using namespace std;
+using LL = long long;
+
+const LL inf = 1e15;
+
+void solve() {
+    LL n, k;
+    cin >> n >> k;
+    vector<LL> a(n + 1, 1);
+    a[1] = 2;
+    a[2] = k + 2;
+    for (int i = 1;i <= n;i ++) cout << a[i] << ' ';
+    cout << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T --) solve();
+}
