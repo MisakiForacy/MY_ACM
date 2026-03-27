257ae3dfc331d05b0d637a25dc1fb32748535bd8
MisakiForacy <fuxuanyu243@gmail.com>
2026-03-27 23:31:53 +0800

    Auto sync 2026/03/27 周五 23:31:53.48

--- HDOJ/2026-3-27/10.cpp
+++ HDOJ/2026-3-27/10.cpp
@@ -17,13 +17,32 @@ void init() {
     for (int i = 1;i <= 63;i ++) p2[i] = p2[i - 1] * 2;
 }
 
+/*
+0
+1 0
+1 1 0
+1 1 1 0 
+1 1 1 1
+1 1 2 0
+1 2 0
+1 2 1
+1 3 0
+2 0
+2 1 0
+2 1 1
+2 2 0 
+3 0
+3 1 0
+4 0
+*/
+
 void solve() {
     LL x, k;
     cin >> x >> k;
     vector<LL> a(x + 1);
     int p = 1;
     for (int i = x;i >= 1;i --) {
-        
+
     }
     for (int i = 1;i < p;i ++) cout << a[i] << ' ';
     cout << '\n';
