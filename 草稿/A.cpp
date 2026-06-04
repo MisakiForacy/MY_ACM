203c6812bec827356d08f7f297200b7d23ed198a
MisakiForacy <fuxuanyu243@gmail.com>
2026-06-04 08:31:29 +0800

    Auto sync 2026-06-04 08:31:29

--- 草稿/A.cpp
+++ 草稿/A.cpp
@@ -4,70 +4,46 @@ using namespace std;
 using LL = long long;
 
 const LL mod = 998244353;
+const int N = 2e5 + 10;
+
+LL qmi(LL a, LL k) {
+    LL res = 1;
+    a %= mod;
+    if (k < 0) return 0;
+    while (k) {
+        if (k & 1) res = res * a % mod;
+        a = a * a % mod;
+        k >>= 1;
+    }
+    return res;
+}
+
+LL fac[N], invfac[N];
+
+void init() {
+    fac[0] = 1;
+    for (int i = 1;i < N;i ++) fac[i] = fac[i - 1] * i % mod;
+    invfac[N - 1] = qmi(fac[N - 1], mod - 2);
+    for (int i = N - 2;i >= 0;i --) invfac[i] = invfac[i + 1] * (i + 1) % mod;
+}
+
+LL C(LL n, LL m) {
+    if (n < m) return 0;
+    return fac[n] * invfac[m] % mod * invfac[n - m] % mod;
+}
+
+void solve() {
+    LL n, m;
+    cin >> n >> m;
+    LL base = C(m, n - 1);
+    LL ans = base * (n - 2) % mod * qmi(2, n - 3) % mod;
+    cout << ans << '\n';
+}
 
 int main() {
+    init();
     ios::sync_with_stdio(0), cin.tie(0);
-    LL n, k;
-    cin >> n >> k;
-    vector<int> p(n + 1);
-    for (int i = 1;i <= n;i ++) {
-        char c;
-        cin >> c;
-        p[i] = c - '0';
-    }
-    int id = 0;
-    vector<vector<LL>> dp(k + 1, vector<LL> (k + 1, 0));
-    dp[0][1] = 1;
-    for (int i = 1;i <= n;i ++) {
-        vector<LL> use(k + 1, 0);
-        LL tot = 0;
-        vector<vector<LL>> ndp(k + 1, vector<LL> (k + 1, 0));
-        for (int j = 0;j <= k;j ++) {
-            for (int t = 1;t < k;t ++) {
-                use[j] += dp[j][t];
-                use[j] %= mod;
-            }
-            tot += use[j];
-            tot %= mod;
-        }
-        if (p[i] == 0) {
-            for (int j = 1;j <= k;j ++) {
-                for (int t = 0;t < k - 1;t ++) {
-                    if (t) {
-                        ndp[j][t + 1] += dp[j][t];
-                        ndp[j][t + 1] %= mod;
-                    } else { 
-                        for (int jj = 0;jj <= k;jj ++) {
-                            if (jj == j) continue;
-                            ndp[j][1] += use[jj];
-                            ndp[j][1] %= mod;
-                        }
-                    }
-                }
-            }
-        } else {
-            for (int j = 0;j <= k;j ++) {
-                if (j == p[i]) {
-                    for (int t = 0;t < k - 1;t ++) {
-                        ndp[p[i]][t + 1] += dp[j][t];
-                        ndp[p[i]][t + 1] %= mod;
-                    }
-                } else {
-                    for (int t = 1;t < k;t ++) {
-                        ndp[p[i]][1] += dp[j][t];
-                        ndp[p[i]][1] %= mod;
-                    }
-                }
-            }
-        }
-        dp = ndp;
-    }
-    LL ans = 0;
-    for (int i = 1;i <= k;i ++) {
-        for (int j = 1;j < k;j ++) {
-            ans += dp[i][j];
-            ans %= mod;
-        }
-    }
-    cout << ans << '\n';
+    int T = 1;
+    // cin >> T;
+    while (T --) solve();
 }
