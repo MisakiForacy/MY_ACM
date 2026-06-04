b4d07cfa48d3726351863ff65b0cbfd3edb968f8
MisakiForacy <fuxuanyu243@gmail.com>
2026-06-05 00:01:44 +0800

    Auto sync 2026-06-05 00:01:44

--- Codeforces/Codeforces Problemset/1210C.cpp
+++ Codeforces/Codeforces Problemset/1210C.cpp
@@ -12,7 +12,7 @@ using LL = long long;
 void solve() {
     int n;
     cin >> n;
-    vector<LL> a(n + 1);
+    vector<LL> a(n + 1), dep(n + 1, 0);
     vector<vector<int>> g(n + 1);
     for (int i = 1;i <= n;i ++) cin >> a[i];
     for (int i = 1;i < n;i ++) {
@@ -21,11 +21,14 @@ void solve() {
         g[u].push_back(v);
         g[v].push_back(u);
     }
+    queue<LL> q;
     vector<vector<LL>> fa(n + 1, vector<LL> (21, 0));
     vector<vector<LL>> gcd(n + 1, vector<LL> (21, 0));
     vector<vector<LL>> sum(n + 1, vector<LL> (21, 0));
     auto dfs = [&](auto dfs, int u, int pu) -> void {
         fa[u][0] = pu;
+        dep[u] = dep[pu] + 1;
+        if (siz(g) == 1 && u != 1) q.push(u);
         for (auto v : g[u]) {
             if (v == pu) continue;
             dfs(dfs, v, u);
@@ -42,11 +45,21 @@ void solve() {
         }
     }
     LL ans = 0;
-    for (int i = 2;i <= n;i ++) {
-        ans += sum[i][1];
-        cout << sum[i][1] << ' ';
+    vector<int> vis(n + 1, 0);
+    while (siz(q)) {
+        int u = q.front();
+        q.pop();
+        if (fa[u][0] != 0) {
+            q.push(fa[u][0]);
+            vis[fa[u][0]] ++;
+        }
+        int len = dep[u];
+        for (int j = 20;j >= 0;j --) {
+            if ((1LL << j) > len) continue;
+            len -= 1LL << j;
+            ans += (1 - vis[u]) * 
+        }
     }
-    cout << '\n';
     cout << ans << '\n';
 }
 
