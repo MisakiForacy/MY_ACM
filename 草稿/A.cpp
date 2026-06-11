097b5fbb5086503193f2c80c3d3ec3d567b24c6f
MisakiForacy <fuxuanyu243@gmail.com>
2026-06-11 21:01:40 +0800

    Auto sync 2026-06-11 21:01:40

--- 草稿/A.cpp
+++ 草稿/A.cpp
@@ -4,17 +4,7 @@ using namespace std;
 using LL = long long;
 
 void solve() {
-  int n;
-  cin >> n;
-  vector<LL> a(n + 1), p(n + 1, 0);
-  for (int i = 1;i <= n;i ++) cin >> a[i];
-  sort(a.begin() + 1, a.end());
-  for (int i = 1;i <= n;i ++) p[i] = p[i - 1] + a[i];
-  LL ans = p[n];
-  for (int i = 1;i <= n;i ++) {
-    ans = min(ans, p[i] + max(p[n] - p[i] - i, 0LL));
-  }
-  cout << ans << '\n';
+  
 }
 
 int main() {
