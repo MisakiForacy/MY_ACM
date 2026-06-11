dacf16b7a2c0d2a07b11a9d5ed70fd2c9a60b53d
MisakiForacy <fuxuanyu243@gmail.com>
2026-06-11 20:00:51 +0800

    Auto sync 2026-06-11 20:00:51

--- 草稿/A.cpp
+++ 草稿/A.cpp
@@ -3,47 +3,33 @@
 using namespace std;
 using LL = long long;
 
-const LL mod = 998244353;
-const int N = 2e5 + 10;
-
-LL qmi(LL a, LL k) {
-    LL res = 1;
-    a %= mod;
-    if (k < 0) return 0;
-    while (k) {
-        if (k & 1) res = res * a % mod;
-        a = a * a % mod;
-        k >>= 1;
-    }
-    return res;
-}
-
-LL fac[N], invfac[N];
-
-void init() {
-    fac[0] = 1;
-    for (int i = 1;i < N;i ++) fac[i] = fac[i - 1] * i % mod;
-    invfac[N - 1] = qmi(fac[N - 1], mod - 2);
-    for (int i = N - 2;i >= 0;i --) invfac[i] = invfac[i + 1] * (i + 1) % mod;
-}
-
-LL C(LL n, LL m) {
-    if (n < m) return 0;
-    return fac[n] * invfac[m] % mod * invfac[n - m] % mod;
-}
-
 void solve() {
-    LL n, m;
-    cin >> n >> m;
-    LL base = C(m, n - 1);
-    LL ans = base * (n - 2) % mod * qmi(2, n - 3) % mod;
-    cout << ans << '\n';
+  int n;
+  cin >> n;
+  vector<int> a, b(n + 1, 0), ans(n + 1, 0);
+  ans[1] = 1;
+  int k, x;
+  cin >> k;
+  for (int i = 1;i <= k;i ++) {
+    cin >> x;
+    b[x] = 1;
+    ans[x] = 1;
+  }
+  for (int i = 1;i <= n - 1;i ++) {
+    cin >> k;
+    for (int j = 1;j <= k;j ++) {
+      cin >> x;
+      if (b[i]) ans[x] = 1;
+    }
+  }
+  int sum = 0;
+  for (int i = 1;i <= n;i ++) sum += ans[i];
+  cout << sum << '\n';
 }
 
 int main() {
-    init();
-    ios::sync_with_stdio(0), cin.tie(0);
-    int T = 1;
-    // cin >> T;
-    while (T --) solve();
+  ios::sync_with_stdio(0), cin.tie(0);
+  int T = 1;
+  // cin >> T;
+  while (T --) solve();
 }
