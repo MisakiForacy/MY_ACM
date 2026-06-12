3db2fc81fe6bbf38d420bb5e354d496de6cafffe
MisakiForacy <fuxuanyu243@gmail.com>
2026-06-13 02:01:17 +0800

    Auto sync 2026-06-13 02:01:17

---
+++ Codeforces/Codeforces Round 1103 (Div. 3)/hackE.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cout << 1 << '\n';
+    cout << 6000 << '\n';
+    for (int i = 1;i <= 6000;i ++) {
+        cout << 1 << " \n"[i == 6000];
+    }
+}
