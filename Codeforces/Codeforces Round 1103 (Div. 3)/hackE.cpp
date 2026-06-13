a9b2b886b23a4088659117cddee418c981813484
MisakiForacy <fuxuanyu243@gmail.com>
2026-06-13 13:33:54 +0800

    Auto sync 2026-06-13 13:33:54

--- Codeforces/Codeforces Round 1103 (Div. 3)/hackE.cpp
+++ Codeforces/Codeforces Round 1103 (Div. 3)/hackE.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main() {
-    cout << 1 << '\n';
-    cout << 6000 << '\n';
-    for (int i = 1;i <= 6000;i ++) {
-        cout << 1 << " \n"[i == 6000];
+    int t = 1;
+    int n = 6000;
+    cout << t << "\n";
+    cout << n << "\n";
+    for (int i = 1; i <= n; ++i) {
+        cout << i << " \n"[i == n];
     }
+    return 0;
 }
