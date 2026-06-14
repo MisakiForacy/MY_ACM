756e10504e2c0332d6400ac08b7f2ac1041f534f
MisakiForacy <fuxuanyu243@gmail.com>
2026-06-14 23:02:03 +0800

    Auto sync 2026-06-14 23:02:03

---
+++ Codeforces/Codeforces Problemset/1394A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using LL = long long;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int n, d, m;
+    cin >> n >> d >> m;
+    
+}
