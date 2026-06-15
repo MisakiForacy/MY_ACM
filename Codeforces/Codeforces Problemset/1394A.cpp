41f099ded4de5ce388bcd6cf676e9ba08a4f78f8
MisakiForacy <fuxuanyu243@gmail.com>
2026-06-15 08:30:30 +0800

    Auto sync 2026-06-15 08:30:30

--- Codeforces/Codeforces Problemset/1394A.cpp
+++ Codeforces/Codeforces Problemset/1394A.cpp
@@ -12,22 +12,7 @@ int main() {
     ios::sync_with_stdio(0), cin.tie(0);
     int n, d, m;
     cin >> n >> d >> m;
-    vector<LL> a, b;
-    for (int i = 1;i <= n;i ++) {
-        int x;
-        cin >> x;
-        if (x >= m) {
-            b.push_back(x);
-        } else {
-            a.push_back(x);
-        }
-    }
-    sort(all(a)), sort(all(b), greater());
-    int l1 = 0, r1 = siz(a) - 1, l2 = 0, r2 = siz(b) - 1;
-    LL ans = 0;
-    while (l1 <= r1 && l2 <= r2) {
-        if (l2 <= r2) {
-            
-        }
-    }
+    vector<LL> a(n);
+    for (int i = 0;i < n;i ++) cin >> a[i];
+    
 }
