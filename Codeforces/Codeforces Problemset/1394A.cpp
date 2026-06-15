3cf382a2959514f5962b83f854b30b2b132cebaf
MisakiForacy <fuxuanyu243@gmail.com>
2026-06-15 10:01:50 +0800

    Auto sync 2026-06-15 10:01:50

--- Codeforces/Codeforces Problemset/1394A.cpp
+++ Codeforces/Codeforces Problemset/1394A.cpp
@@ -12,10 +12,16 @@ int main() {
     ios::sync_with_stdio(0), cin.tie(0);
     int n, d, m;
     cin >> n >> d >> m;
-    vector<LL> a(n);
+    vector<LL> a(n + 1), p(n + 1, 0), p1(1, 0);
     for (int i = 0;i < n;i ++) cin >> a[i];
-    sort(all(a), greater());
+    sort(a.begin() + 1, a.end(), greater());
+    for (int i = 1;i <= n;i ++) if (a[i] < m) p1.push_back(p1.back() + a[i]);
+    int t = siz(p1) - 1;
+    for (int i = 1;i <= n;i ++) p[i] = p[i - 1] + a[i];
+    LL ans = 0;
     for (int i = 0;i < n;i ++) {
-        if (a[i] > )
+        if (a[i] < m) break;
+        LL val = 0;
+        val += (p[n] - p[i - 1]) 
     }
 }
