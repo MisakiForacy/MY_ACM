9341497952f623ab1a6ff561664f6c73f2c4f636
MisakiForacy <fuxuanyu243@gmail.com>
2026-06-17 00:01:27 +0800

    Auto sync 2026-06-17 00:01:27

--- 草稿/test.cpp
+++ 草稿/test.cpp
@@ -10,5 +10,21 @@ const LL inf = 1e15;
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0);
-    
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> p(n + 1, vector<int> (m + 1));
+    for (int i = 1;i <= n;i ++) {
+        for (int j = 1;j <= m;j ++) {
+            char c;
+            cin >> c;
+            p[i][j] = (c == 'w');
+        }
+    }
+    for (int i = 1;i <= n;i ++) {
+        for (int j = 1;j <= m;j ++) {
+            p[i][j] += p[i - 1][j] + p[i][j - 1] - p[i - 1][j - 1];
+        }
+    }
+    int l = 0, r = m, u = 0, d = n;
+    while (p[d][l + 1] - p[u][0])
 }
