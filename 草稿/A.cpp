c99584a28a5ccb1761668e8c3feb01fe6a767d92
MisakiForacy <fuxuanyu243@gmail.com>
2026-06-16 23:37:30 +0800

    Auto sync 2026-06-16 23:37:30

--- 草稿/A.cpp
+++ 草稿/A.cpp
@@ -1,54 +1,14 @@
 #include <bits/stdc++.h>
 
-#define siz(x) ((int)x.size())
 #define all(x) begin(x),end(x)
 
+#define siz(x) ((int)x.size())
+
 using namespace std;
+
 using LL = long long;
 
-struct P 
-{
-  LL id, v, ans;
-  bool operator < (const P & T) const {
-    return id < T.id;
-  }
-};
-
-void solve() {
-  int n;
-  cin >> n;
-  vector<P> a(n);
-  // cout << "ok\n";
-  vector<int> use;
-  for (int i = 0;i < n;i ++) {
-    cin >> a[i].v;
-    a[i].id = i;
-    use.push_back(a[i].v);
-  }
-  sort(all(a), [&](P x, P y) {
-    return x.v < y.v;    
-  });
-  sort(all(use));
-  LL ans = 0;
-  for (int i = 0;i < n;i ++) {
-    ans += (a[i].v - a[0].v + 1);
-  }
-  // cout << ans << '\n';
-  a[0].ans = ans;
-  for (int i = 1;i < n;i ++) {
-    LL pre = i;
-    LL suf = n - pre;
-    LL len = a[i].v - a[i - 1].v;
-    a[i].ans = a[i - 1].ans - suf * len + pre * len;
-  }
-  sort(all(a)); 
-  for (int i = 0;i < n;i ++) cout << a[i].ans << ' ';
-  cout << '\n';
-}
-
 int main() {
-  ios::sync_with_stdio(0), cin.tie(0);
-  int T = 1;
-  cin >> T;
-  while (T --) solve();
+    ios::sync_with_stdio(0), cin.tie(0);
+    
 }
--- 草稿/test.cpp
+++ 草稿/test.cpp
@@ -9,28 +9,6 @@ using LL = long long;
 const LL inf = 1e15;
 
 int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<int> f(n + 1);
-    for (int i = 1;i <= n;i ++) f[i] = i;
-    auto find = [&](auto find, int x) -> int {
-        return (f[x] == x ? x : f[x] = find(find, f[x]));
-    };
-    auto merge = [&](int x, int y) -> void {
-        x = find(find, x);
-        y = find(find, y);
-        if (x == y) return;
-        if (x < y) swap(x, y);
-        f[x] = y;
-    };
-    for (int i = 1;i <= m;i ++) {
-        int x, y, z;
-        cin >> x >> y >> z;
-        merge(x, y);
-    }
-    set<int> p;
-    for (int i = 1;i <= n;i ++) {
-        p.insert(find(find, i));
-    }
-    cout << siz(p) << '\n';
+    ios::sync_with_stdio(0), cin.tie(0);
+    
 }
