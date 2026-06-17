a539d4295ce997479d6d784e9e53c20055c3944e
MisakiForacy <fuxuanyu243@gmail.com>
2026-06-17 19:07:28 +0800

    Auto sync 2026-06-17 19:07:28

--- week/wl20260617/J.cpp
+++ week/wl20260617/J.cpp
@@ -1,15 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n, k, ans;
+string s, t;
+int check() {
+    int x = 0, y = 0;
+    for (int i = 0;i < n;i ++) {
+        if (s[i] == t[1]) y += x;
+        if (s[i] == t[0]) x ++;
+    }
+    return y;
+}
+void dfs(string ss, int p, int kk) {
+    if (!kk || p == n) {
+        ans = max(ans, check());
+        return;
+    }
+    if (ss[p] == t[0]) {
+        string t1 = ss;
+        t1[p] = t[1]; 
+        dfs(ss, p + 1, kk);
+        dfs(t1, p + 1, kk - 1);
+    }
+    if (ss)
+}
 int main() {
     int t;
     cin >> t;
     while (t--) {
-        int n, k;
         cin >> n >> k;
-        string s, t;
         cin >> s >> t;
-        for (int i = 0;i < s.size();i ++) {
-            
-        }
+        ans = 0;
+        dfs(s, 0, k);
+        cout << ans << '\n';
     }   
 }
