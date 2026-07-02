40e50093491d8b8c88ee47a63dc53fa95540f310
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-02 20:31:13 +0800

    Auto sync 2026-07-02 20:31:13

--- Codeforces/Codeforces Round 1107 (Div. 3)/D.cpp
+++ Codeforces/Codeforces Round 1107 (Div. 3)/D.cpp
@@ -10,33 +10,18 @@ using LL = long long;
 void solve() {
     int n;
     cin >> n;
-    vector<LL> a(n + 1, 0), b(n + 1, 0);
+    vector<LL> a(n + 1, 0), b(n + 1, 0), c(n + 1, 0);
     for (int i = 1;i <= n;i ++) cin >> a[i];
     for (int i = 1;i <= n;i ++) cin >> b[i];
-    int ok = 1;
-    int s0 = 0, s1 = 0;
-    for (int i = 1;i <= n;i ++) {
-        cout << a[i] << ':' << ok << ' ' << s0 << ' ' << s1 << '\n';
-        if (i & 1) {
-            a[i] += s1;
-            if (a[i] > b[i]) {
-                ok = 0;
-            } else {
-                s1 += b[i] - a[i];
-                s0 -= b[i] - a[i];
-            }
-        } else {
-            a[i] += s0;
-            if (a[i] > b[i]) {
-                ok = 0;
-            } else {
-                s0 += b[i] - a[i];
-                s1 -= b[i] - a[i];
-            }
-        } 
-    }
+    LL dat = 0;
+    for (int i = 1;i <= n;i ++) cout << b[i] - a[i] << ' ';
     cout << '\n';
-    cout << (ok ? "YES\n" : "NO\n");
+    for (int i = 1;i <= n;i ++) dat += b[i] - a[i];
+    if (dat < 0) {
+        cout << "NO\n";
+    } else {
+        cout << "YES\n";
+    }
 }
 
 int main() {
