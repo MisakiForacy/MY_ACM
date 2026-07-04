0f321b2a6213360d334a53942d4fa2182f7f3d5b
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-04 23:01:15 +0800

    Auto sync 2026-07-04 23:01:15

--- Atcoder/AtCoder Beginner Contest 465/E.cpp
+++ Atcoder/AtCoder Beginner Contest 465/E.cpp
@@ -1,9 +1,25 @@
 #include <bits/stdc++.h>
 
+#define all(x) begin(x),end(x)
+
+#define siz(x) ((int)x.size())
+
 using namespace std;
 
 using LL = long long;
 
+int f[10][500][3][2][1024];
+
+void solve() {
+    string N;
+    cin >> N; 
+    int n = siz(N);
+    cout << n << '\n';
+}
+
 int main() {
     ios::sync_with_stdio(0), cin.tie(0);
+    int T = 1;
+    // cin >> T;
+    while (T --) solve();
 }
