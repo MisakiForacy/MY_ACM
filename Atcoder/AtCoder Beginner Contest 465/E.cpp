b7658d937740e59f6225b9a46bf471a0f15cbbdf
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-05 17:31:11 +0800

    Auto sync 2026-07-05 17:31:11

--- Atcoder/AtCoder Beginner Contest 465/E.cpp
+++ Atcoder/AtCoder Beginner Contest 465/E.cpp
@@ -8,13 +8,53 @@ using namespace std;
 
 using LL = long long;
 
-int f[10][500][3][2][1024];
+LL f[501][2][3][1024];
+// f[位数][lim][模三][bit]
 
 void solve() {
     string N;
     cin >> N; 
     int n = siz(N);
-    cout << n << '\n';
+    reverse(all(N));
+    N = ' ' + N;
+    vector<int> p(n + 1);
+    for (int i = 1;i <= n;i ++) p[i] = N[i] - '0';
+    memset(f, 0, sizeof(f));
+    f[0][0][0][0] = 1;
+    LL ans = 0;
+    for (int i = 1;i <= n;i ++) {
+        for (int b = 0;b < (1 << 10);b ++) {
+            for (int x = 0;x <= (i == 1 ? p[i] : 9);x ++) {
+                for (int m = 0;m < 3;m ++) {
+                    if (b == 0 && x == 0) continue;
+                    f[i][(i == 1 && x == p[i])][(m + x) % 3][b | (1 << x)] ++;
+                }
+            }
+        }
+        for (int lim = 0;lim <= 1;lim ++) {
+            for (int b = 0;b < (1 << 10);b ++) {
+                for (int x = 0;x <= (lim ? p[i] : 9);x ++) {
+                    for (int m = 0;m < 3;m ++) {
+                        if (b == 0 && x == 0) {
+                            f[i][lim && x == p[i]][(m + x) % 3][b] += f[i - 1][lim][m][b];
+                        } else {
+                            f[i][lim && x == p[i]][(m + x) % 3][b | (1 << x)] += f[i - 1][lim][m][b];
+                        }
+                    }
+                }
+            }
+        }
+    }
+    for (int lim = 0;lim <= 1;lim ++) {
+        for (int b = 1;b < (1LL << 10);b ++) {
+            for (int m = 0;m < 3;m ++) {
+                if (m && (b >> 3 & 1) && __builtin_popcountll(b) != 3) ans += f[n][lim][m][b];
+                if (!m && !(b >> 3 & 1) && __builtin_popcountll(b) != 3) ans += f[n][lim][m][b];
+                if (m && !(b >> 3 & 1) && __builtin_popcountll(b) == 3) ans += f[n][lim][m][b];
+            }
+        }
+    }
+    cout << ans << '\n';
 }
 
 int main() {
