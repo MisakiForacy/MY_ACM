fb9632780bbd4966912d951d4bd1da0c91dd835a
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-08 23:00:48 +0800

    Auto sync 2026-07-08 23:00:48

--- Atcoder/AtCoder Beginner Contest 465/E.cpp
+++ Atcoder/AtCoder Beginner Contest 465/E.cpp
@@ -22,11 +22,11 @@ void solve() {
     for (int i = 1;i <= n;i ++) p[i] = N[i] - '0';
     memset(f, 0, sizeof(f));
     LL ans = 0;
-    f[0][1][0][0] = 1;
+    // f[0][1][0][0] = 1;
     for (int i = 0;i < n;i ++) {
-        // for (int x = 0;x <= (i == 0 ? p[1] : 9);x ++) {
-        //     f[i + 1][(i == 0 && x == p[i + 1])][x % 3][(x ? 1 << x : 0)] ++;
-        // }
+        for (int x = 0;x <= (i == 0 ? p[1] : 9);x ++) {
+            f[i + 1][(i == 0 && x == p[i + 1])][x % 3][(x ? 1 << x : 0)] ++;
+        }
         for (int lim = 0;lim <= 1;lim ++) {
             for (int x = 0;x <= (lim ? p[i + 1] : 9);x ++) {
                 for (int b = 0;b < (1 << 10);b ++) {
