44ae3559a321af7abe0c172b547866c7c75a2c32
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-08 20:00:51 +0800

    Auto sync 2026-07-08 20:00:51

--- Atcoder/AtCoder Beginner Contest 465/E.cpp
+++ Atcoder/AtCoder Beginner Contest 465/E.cpp
@@ -8,6 +8,8 @@ using namespace std;
 
 using LL = long long;
 
+const LL mod = 998244353;
+
 LL f[501][2][3][1024];
 // f[位数][lim][模三][bit]
 
@@ -20,16 +22,18 @@ void solve() {
     for (int i = 1;i <= n;i ++) p[i] = N[i] - '0';
     memset(f, 0, sizeof(f));
     LL ans = 0;
+    f[0][1][0][0] = 1;
     for (int i = 0;i < n;i ++) {
-        for (int x = 0;x <= (i == 0 ? p[1] : 9);x ++) {
-            f[i + 1][(i == 0 && x == p[i + 1])][x % 3][(x ? 1 << x : 0)] ++;
-        }
+        // for (int x = 0;x <= (i == 0 ? p[1] : 9);x ++) {
+        //     f[i + 1][(i == 0 && x == p[i + 1])][x % 3][(x ? 1 << x : 0)] ++;
+        // }
         for (int lim = 0;lim <= 1;lim ++) {
             for (int x = 0;x <= (lim ? p[i + 1] : 9);x ++) {
                 for (int b = 0;b < (1 << 10);b ++) {
                     for (int m = 0;m < 3;m ++) {
                         int nb = (b || x ? b | (1 << x) : 0);
                         f[i + 1][(lim && x == p[i + 1])][(m + x) % 3][nb] += f[i][lim][m][b];
+                        f[i + 1][(lim && x == p[i + 1])][(m + x) % 3][nb] %= mod;
                     }
                 }
             }
@@ -44,7 +48,8 @@ void solve() {
                 if (b >> 3 & 1) p ++;
                 if (p == 1 && f[n][lim][m][b]) {
                     ans += f[n][lim][m][b];
-                    cout << n << ' ' << lim << ' ' << m << ' ' << b << '\n';
+                    ans %= mod;
+                    // cout << n << ' ' << lim << ' ' << m << ' ' << b << '\n';
                 }
             }
         }
