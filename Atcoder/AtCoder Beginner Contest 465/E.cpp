440b0e96e0a46a237f14352f80b0e7b0a04919f4
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-08 23:31:53 +0800

    Auto sync 2026-07-08 23:31:53

--- Atcoder/AtCoder Beginner Contest 465/E.cpp
+++ Atcoder/AtCoder Beginner Contest 465/E.cpp
@@ -21,24 +21,23 @@ void solve() {
     vector<int> p(n + 1);
     for (int i = 1;i <= n;i ++) p[i] = N[i] - '0';
     memset(f, 0, sizeof(f));
-    LL ans = 0;
-    // f[0][1][0][0] = 1;
+    f[0][1][0][0] = 1;
     for (int i = 0;i < n;i ++) {
-        for (int x = 0;x <= (i == 0 ? p[1] : 9);x ++) {
-            f[i + 1][(i == 0 && x == p[i + 1])][x % 3][(x ? 1 << x : 0)] ++;
-        }
         for (int lim = 0;lim <= 1;lim ++) {
             for (int x = 0;x <= (lim ? p[i + 1] : 9);x ++) {
                 for (int b = 0;b < (1 << 10);b ++) {
                     for (int m = 0;m < 3;m ++) {
-                        int nb = (b || x ? b | (1 << x) : 0);
-                        f[i + 1][(lim && x == p[i + 1])][(m + x) % 3][nb] += f[i][lim][m][b];
-                        f[i + 1][(lim && x == p[i + 1])][(m + x) % 3][nb] %= mod;
+                        int nlim = lim && x == p[i + 1];
+                        int nm = (m + x) % 3;
+                        int nb = b || x ? b | (1 << x) : 0;
+                        f[i + 1][nlim][nm][nb] += f[i][lim][m][b];
+                        f[i + 1][nlim][nm][nb] %= mod;
                     }
                 }
             }
         }
     }
+    LL ans = 0;
     for (int b = 1;b < (1 << 10);b ++) {
         for (int m = 0;m < 3;m ++) {
             for (int lim = 0;lim <= 1;lim ++) {
@@ -49,7 +48,6 @@ void solve() {
                 if (p == 1 && f[n][lim][m][b]) {
                     ans += f[n][lim][m][b];
                     ans %= mod;
-                    // cout << n << ' ' << lim << ' ' << m << ' ' << b << '\n';
                 }
             }
         }
