5940eb200301af2d33aec94a100cd27f29d6dafb
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-08 19:00:44 +0800

    Auto sync 2026-07-08 19:00:44

--- Atcoder/AtCoder Beginner Contest 465/E.cpp
+++ Atcoder/AtCoder Beginner Contest 465/E.cpp
@@ -20,35 +20,32 @@ void solve() {
     for (int i = 1;i <= n;i ++) p[i] = N[i] - '0';
     memset(f, 0, sizeof(f));
     LL ans = 0;
-    for (int i = 1;i <= n;i ++) {
-        for (int b = 0;b < (1 << 10);b ++) {
-            if (__builtin_popcountll(b) >= i) continue;
-            for (int x = 0;x <= (i == 1 ? p[i] : 9);x ++) {
-                if (b == 0 && x == 0) continue;
-                f[i][(i == 1 && x == p[i])][x % 3][b | (1 << x)] ++;
-            }
+    for (int i = 0;i < n;i ++) {
+        for (int x = 0;x <= (i == 0 ? p[1] : 9);x ++) {
+            f[i + 1][(i == 0 && x == p[i + 1])][x % 3][(x ? 1 << x : 0)] ++;
         }
         for (int lim = 0;lim <= 1;lim ++) {
-            for (int b = 0;b < (1 << 10);b ++) {
-                if (__builtin_popcountll(b) >= i) continue;
-                for (int x = 0;x <= (lim ? p[i] : 9);x ++) {
+            for (int x = 0;x <= (lim ? p[i + 1] : 9);x ++) {
+                for (int b = 0;b < (1 << 10);b ++) {
                     for (int m = 0;m < 3;m ++) {
-                        if (b == 0 && x == 0) {
-                            f[i][lim && x == p[i]][(m + x) % 3][b] += f[i - 1][lim][m][b];
-                        } else {
-                            f[i][lim && x == p[i]][(m + x) % 3][b | (1 << x)] += f[i - 1][lim][m][b];
-                        }
+                        int nb = (b || x ? b | (1 << x) : 0);
+                        f[i + 1][(lim && x == p[i + 1])][(m + x) % 3][nb] += f[i][lim][m][b];
                     }
                 }
             }
         }
     }
-    for (int lim = 0;lim <= 1;lim ++) {
-        for (int b = 0;b < (1LL << 10);b ++) {
-            for (int m = 0;m < 3;m ++) {
-                if (m && (b >> 3 & 1) && __builtin_popcountll(b) != 3) ans += f[n][lim][m][b];
-                if (!m && !(b >> 3 & 1) && __builtin_popcountll(b) != 3) ans += f[n][lim][m][b];
-                if (m && !(b >> 3 & 1) && __builtin_popcountll(b) == 3) ans += f[n][lim][m][b];
+    for (int b = 1;b < (1 << 10);b ++) {
+        for (int m = 0;m < 3;m ++) {
+            for (int lim = 0;lim <= 1;lim ++) {
+                int p = 0;
+                if (m == 0) p ++;
+                if (__builtin_popcount(b) == 3) p ++;
+                if (b >> 3 & 1) p ++;
+                if (p == 1 && f[n][lim][m][b]) {
+                    ans += f[n][lim][m][b];
+                    cout << n << ' ' << lim << ' ' << m << ' ' << b << '\n';
+                }
             }
         }
     }
