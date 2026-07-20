9bd5ddac86a94d39b21796d3242482d04dfe1660
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-21 01:31:18 +0800

    Auto sync 2026-07-21 01:31:18

--- Atcoder/AtCoder Beginner Contest 467/DFS.cpp
+++ Atcoder/AtCoder Beginner Contest 467/DFS.cpp
@@ -6,28 +6,15 @@ using LL = long long;
 
 const int N = 15;
 
-int n;
+int n, m;
 
 int a[N], vis[N];
 
-void dfs(int pos) {
-    if (pos > n) {
-        for (int i = 1;i <= n;i ++) {
-            cout << a[i] << ' ';
-        }
-        cout << '\n';
-        return;
-    }
-    for (int i = 1;i <= n;i ++) {
-        if (vis[i]) continue;
-        vis[i] = 1;
-        a[pos] = i;
-        dfs(pos + 1);
-        vis[i] = 0;
-    }
+void dfs(int pos, int x) {
+    
 }
 
 int main() {
-    cin >> n;
-    dfs(1);
+    cin >> n >> m;
+    dfs(1, 1);
 }
