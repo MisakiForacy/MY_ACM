cc19500716370ceac70eb4734bebf279636c1211
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-20 21:34:53 +0800

    Auto sync 2026-07-20 21:34:53

--- Atcoder/AtCoder Beginner Contest 467/D.cpp
+++ Atcoder/AtCoder Beginner Contest 467/D.cpp
@@ -5,7 +5,28 @@ using namespace std;
 using LL = long long;
 
 void solve() {
-    LL px, py, 
+    LL px, py, qx, qy, rx, ry, sx, sy;
+    cin >> px >> py >> qx >> qy >> rx >> ry >> sx >> sy;
+    LL dx1 = qx - px;
+    LL dy1 = qy - py;
+    LL dx2 = sx - rx;
+    LL dy2 = sy - ry;
+    if (dx1 * dy2 != dy1 * dx2) {
+        cout << "Yes\n";
+    } else {
+        px *= 2, py *= 2, qx *= 2, qy *= 2, rx *= 2, ry *= 2, sx *= 2, sy *= 2;
+        LL tx = (qx + px) / 2;
+        LL ty = (qy + py) / 2;
+        LL d1 = (tx - rx) * (tx - rx) + (ty - ry) * (ty - ry);
+        LL d2 = (tx - sx) * (tx - sx) + (ty - sy) * (ty - sy);
+        // cout << tx << ' ' << ty << '\n';
+        // cout << d1 << ' ' << d2 << '\n';
+        if (d1 == d2) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+    }
 }
 
 int main() {
---
+++ Atcoder/AtCoder Beginner Contest 467/E.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using LL = long long;
+
+void solve() {
+    vector<LL> a(n + 1, 0), b(n, 0)
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T --) solve();
+}
