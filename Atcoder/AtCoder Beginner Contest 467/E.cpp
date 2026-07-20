7b0b911312a7bae84744c04ef04fb0d604f56b1c
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-21 00:04:47 +0800

    Auto sync 2026-07-21 00:04:47

--- Atcoder/AtCoder Beginner Contest 467/E.cpp
+++ Atcoder/AtCoder Beginner Contest 467/E.cpp
@@ -7,21 +7,21 @@ using LL = long long;
 void solve() {
     LL n, m;
     cin >> n >> m;
-    vector<LL> a(n + 1, 0), b(n, 0), c(n, 0);
+    vector<LL> a(n + 1, 0), b(n, 0), c(n + 1, 0);
     for (int i = 1;i <= n;i ++) cin >> a[i];
     for (int i = 1;i < n;i ++) cin >> b[i];
     int l = 0, r = m;
     auto check = [&](int x) -> bool {
         c[1] = x;
-        LL sum1 = 0;
+        LL sum1 = c[1];
         for (int i = 2;i < n;i ++) {
             c[i] = (b[i - 1] - (c[i - 1] + a[i - 1] + a[i]) % m);
             if (c[i] < 0) c[i] += m;
             sum1 += c[i];
         }
         c[1] = x + 1;
-        LL sum2 = 0;
-        for (int i = 2;i < n;i ++) {
+        LL sum2 = c[1];
+        for (int i = 2;i <= n;i ++) {
             c[i] = (b[i - 1] - (c[i - 1] + a[i - 1] + a[i]) % m);
             if (c[i] < 0) c[i] += m;
             sum2 += c[i];
@@ -35,15 +35,21 @@ void solve() {
         else
             r = mid;
     }
-    cout << l << '\n';
-    for (l = 0;l < m;l ++) {LL ans = 0;
-    c[1] = l + 1;
-    for (int i = 2;i < n;i ++) {
-        c[i] = (b[i - 1] - (c[i - 1] + a[i - 1] + a[i]) % m);
-        if (c[i] < 0) c[i] += m;
-        ans += c[i];
-    }}
-    cout << ans << '\n';
+    // cout << l << '\n';
+    // for (l = 0;l < m;l ++) {
+        c[1] = l;
+        LL ans = c[1];
+        for (int i = 2;i <= n;i ++) {
+            c[i] = (b[i - 1] - (c[i - 1] + a[i - 1] + a[i]) % m);
+            if (c[i] < 0) c[i] += m;
+            ans += c[i];
+        }
+        // for (int i = 1;i <= n;i ++) {
+        //     cout << c[i] << ' ';
+        // }
+        // cout << '\n';
+        cout << ans << '\n';
+    // }
 }
 
 int main() {
