a13ffba72c3fdbedacc1e53e9de4b3c6467e40e4
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-21 10:00:58 +0800

    Auto sync 2026-07-21 10:00:58

--- Atcoder/AtCoder Beginner Contest 467/DFS.cpp
+++ Atcoder/AtCoder Beginner Contest 467/DFS.cpp
@@ -4,34 +4,37 @@ using namespace std;
 
 const int N = 15;
 
-vector<int> g[N];
-vector<vector<int>> g(N);
+int n, m;
+int a[N];
 
-vector<int> vis(N);
-
-void dfs(int u) {
-    vis[u] = 1;
-    for (int v : g[u]) {
-        if (!vis[v]) 
-            dfs(v);
+void dfs(int pos, int x) {
+    if (pos > m) {
+        for (int i = 1;i <= m;i ++) 
+            cout << a[i] << ' ';
+        cout << '\n';
+        return;
     }
-}
-
-const int dx[] = {0, 1, 0, -1};
-const int dy[] = {1, 0, -1, 0};
-
-int vis[N][N], mp[N][N];
-
-void dfs(int x, int y) {
-    vis[x][y] = 1;
-    for (int k = 0;k < 4;k ++) {
-        int nx = x + dx[k], ny = y + dy[k];
-        if (!vis[nx][ny] && !mp[nx][ny]) {
-            dfs(nx, ny);
-        }
+    for (int i = x + 1;i <= n;i ++) {
+        a[pos] = i;
+        dfs(pos + 1, i);
     }
 }
 
 int main() {
-    
+    cin >> n >> m;
+    dfs(1, 0);
 }
+
+/*
+5 3
+1 2 3 
+1 2 4 
+1 2 5 
+1 3 4 
+1 3 5 
+1 4 5 
+2 3 4 
+2 3 5 
+2 4 5 
+3 4 5 
+*/
