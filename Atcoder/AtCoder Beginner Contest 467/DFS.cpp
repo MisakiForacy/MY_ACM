d7b4690fa06570c7d7df3a669db681dfa0761872
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-21 13:31:09 +0800

    Auto sync 2026-07-21 13:31:09

--- Atcoder/AtCoder Beginner Contest 467/DFS.cpp
+++ Atcoder/AtCoder Beginner Contest 467/DFS.cpp
@@ -4,11 +4,47 @@ using namespace std;
 
 using LL = long long;
 
+const int N = 30;
 
+const int off = N;
+
+int ans = 0;
+int n;
+int a[N], px[2 * N], qx[2 * N], h[N];
+
+void dfs(int pos) {
+    if (pos > n) {
+        if (!ans) {
+            for (int i = 1;i <= n;i ++) {
+                cout << a[i] << ' ';
+            }
+            cout << '\n';
+        }
+        ans ++;
+    }
+    for (int i = 1;i <= n;i ++) {
+        if (!h[i] && !px[pos - i + off] && !qx[pos + i]) {
+            h[i] = 1;
+            px[pos - i + off] = 1;
+            qx[pos + i] = 1;
+            a[pos] = i;
+            dfs(pos + 1);
+            h[i] = 0;
+            px[pos - i + off] = 0;
+            qx[pos + i] = 0;
+        }
+    }
+}
+
+void solve() {
+    cin >> n;
+    dfs(1);
+    cout << ans << '\n';
+}
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0);
     int T = 1;
-    cin >> T;
+    // cin >> T;
     while (T --) solve();
 }
