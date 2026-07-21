4ed134bef297dba37ad8364f123edf7fcbd08ee0
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-21 09:34:22 +0800

    Auto sync 2026-07-21 09:34:22

--- Atcoder/AtCoder Beginner Contest 467/DFS.cpp
+++ Atcoder/AtCoder Beginner Contest 467/DFS.cpp
@@ -1,69 +1,37 @@
 #include <bits/stdc++.h>
 
-#define x first 
-#define y second
-
-#define all(x) begin(x),end(x)
-#define siz(x) ((int)x.size())
-
 using namespace std;
-using LL = long long;
+
+const int N = 15;
+
+vector<int> g[N];
+vector<vector<int>> g(N);
+
+vector<int> vis(N);
+
+void dfs(int u) {
+    vis[u] = 1;
+    for (int v : g[u]) {
+        if (!vis[v]) 
+            dfs(v);
+    }
+}
 
 const int dx[] = {0, 1, 0, -1};
 const int dy[] = {1, 0, -1, 0};
 
-const int N = 105;
+int vis[N][N], mp[N][N];
 
-int a[N][N], vis[N][N], ans[N], idx[N][N];
-
-void bfs(int x, int y, int t) {
-    queue<pair<int, int>> q;
-    q.push({x, y});
+void dfs(int x, int y) {
     vis[x][y] = 1;
-    idx[x][y] = t;
-    ans[t] ++;
-    while (q.size()) {
-        auto [x, y] = q.front();
-        q.pop();
-        for (int k = 0;k < 4;k ++) {
-            int nx = x + dx[k], ny = y + dy[k];
-            if (!vis[nx][ny] && !a[nx][ny]) {
-                vis[nx][ny] = 1;
-                idx[nx][ny] = t;
-                ans[t] ++;
-                q.push({nx, ny});
-            }
+    for (int k = 0;k < 4;k ++) {
+        int nx = x + dx[k], ny = y + dy[k];
+        if (!vis[nx][ny] && !mp[nx][ny]) {
+            dfs(nx, ny);
         }
     }
 }
 
-void solve() {
-    int n, m;
-    cin >> n >> m;
-    for (int i = 0;i <= n + 1;i ++) 
-        for (int j = 0;j <= n + 1;j ++)
-            a[i][j] = 1;
-    for (int i = 1;i <= n;i ++) 
-        for (int j = 1;j <= n;j ++) {
-            char c;
-            cin >> c;
-            a[i][j] = c == '#';
-        }
-    int t = 0;
-    for (int i = 1;i <= n;i ++) 
-        for (int j = 1;j <= n;j ++) 
-            if (!vis[i][j])
-                bfs(i, j, ++ t);
-    while (m --) {
-        int x, y;
-        cin >> x >> y;
-        cout << ans[idx[x][y]] << '\n';
-    } 
-}
-
 int main() {
-    ios::sync_with_stdio(0), cin.tie(0);
-    int T = 1;
-    // cin >> T;
-    while (T --) solve();
+    
 }
