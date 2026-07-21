e86d94997d1575700f6e11f18b81df07849623b2
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-21 14:30:57 +0800

    Auto sync 2026-07-21 14:30:57

--- Atcoder/AtCoder Beginner Contest 467/test.cpp
+++ Atcoder/AtCoder Beginner Contest 467/test.cpp
@@ -11,47 +11,37 @@ void solve() {
     for (int i = 1;i <= n;i ++) cin >> a[i];
     for (int i = 1;i <= n;i ++) pre[i] = pre[i - 1] + a[i];
     for (int i = n;i >= 1;i --) suf[i] = suf[i + 1] + a[i];
-    if (n == 1) {
-        cout << "NO\n";
-    } else if (n == 2) {
-        cout << "YES\n";
-    } else {
-        int mid = n / 2 + 1;
-        LL Ls = 0, Rs = 0;
-        for (int i = 1;i <= n;i ++) {
-            if (i < mid) 
-                Ls += a[i];
-            if (i > mid)
-                Rs += a[i];
-        }
-        if (Ls > Rs) {
-            cout << "YES\n";
-        } else if (Ls < Rs) {
-            cout << "NO\n";
-        } else {
-            int ok, l = 1, r = n;
-            if (n & 1) {
-                ok = 0;
-                while (l < r) {
-                    if (pre[l] != suf[r]) {
-                        ok = (pre[l] > suf[r]);
-                        break;
-                    }
-                    l ++, r --;
-                }
-            } else {
-                ok = 1, l = 2;
-                while (l < r) {
-                    if (pre[l] != suf[r]) {
-                        ok = !(pre[l] < suf[r]);
-                        break;
-                    }
-                    l ++, r --;
-                }
+    int mid = n / 2 + 1;
+    LL Ls = 0, Rs = 0;
+    for (int i = 1;i <= n;i ++) {
+        if (i < mid) 
+            Ls += a[i];
+        if (i > mid)
+            Rs += a[i];
+    }
+    int ok, l = 1, r = n;
+    if (n & 1) {
+        ok = 0;
+        l = mid - 1, r = mid + 1;
+        while (l >= 1 && r <= n) {
+            if (pre[l] != suf[r]) {
+                if (pre[l] > suf[r]) ok = 1;
+                break;
             }
-            cout << (ok ? "YES\n" : "NO\n");
+            l --, r ++;
+        }
+    } else {
+        ok = 1;
+        l = mid - 1, r = mid + 1;
+        while (l >= 1 && r <= n) {
+            if (pre[l] != suf[r]) {
+                if (pre[l] < suf[r]) ok = 0;
+                break;
+            }
+            l --, r ++;
         }
     }
+    cout << (ok ? "YES\n" : "NO\n");
 }
 
 int main() {
