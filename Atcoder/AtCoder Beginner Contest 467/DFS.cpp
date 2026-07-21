673b0897a42f7c874032ebc4fcd2118f247fc1cb
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-21 10:31:25 +0800

    Auto sync 2026-07-21 10:31:25

--- Atcoder/AtCoder Beginner Contest 467/DFS.cpp
+++ Atcoder/AtCoder Beginner Contest 467/DFS.cpp
@@ -1,40 +1,35 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-const int N = 15;
-
-int n, m;
-int a[N];
-
-void dfs(int pos, int x) {
-    if (pos > m) {
-        for (int i = 1;i <= m;i ++) 
-            cout << a[i] << ' ';
-        cout << '\n';
+int used[20],a[20],n;
+long long ret=0;
+bool flag;
+void dfs(int x)
+{
+    int i;
+    if(x > n)
+    {
+        flag=1;
+        for(i=1;i<=n;i++)
+            if(a[i]+i > n+2)
+            {
+                flag=0;
+                break;
+            }
+        if(flag) ret++;
         return;
     }
-    for (int i = x + 1;i <= n;i ++) {
-        a[pos] = i;
-        dfs(pos + 1, i);
-    }
+    for(i=1;i<=n;i++)
+        if(used[i]==0)
+        {
+            used[i]=1, a[x]=i;
+            dfs(x+1);
+            used[i]=0, a[x]=0;
+        }
 }
-
-int main() {
-    cin >> n >> m;
-    dfs(1, 0);
+int main()
+{
+    cin>>n;
+    dfs(1);
+    cout<<ret;
+    return 0;
 }
-
-/*
-5 3
-1 2 3 
-1 2 4 
-1 2 5 
-1 3 4 
-1 3 5 
-1 4 5 
-2 3 4 
-2 3 5 
-2 4 5 
-3 4 5 
-*/
