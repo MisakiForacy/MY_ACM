d3fbc718b904d0aa6ee828f3da38fbc3464c512f
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-21 11:31:24 +0800

    Auto sync 2026-07-21 11:31:24

--- Atcoder/AtCoder Beginner Contest 467/DFS.cpp
+++ Atcoder/AtCoder Beginner Contest 467/DFS.cpp
@@ -13,18 +13,16 @@ void solve() {
     int n, d;
     cin >> n >> d;
     vector<LL> v(n), a(n + 1), p(n + 1);
-    for (int i = 1;i < n;i ++) cin >> v[i], p[i + 1] = p[i] + v[i];
+    for (int i = 1;i < n;i ++) cin >> v[i];
     for (int i = 1;i <= n;i ++) cin >> a[i];
-    int nxt = 2;
-    LL ans = 0, sum = 0;
-    for (int i = 1;i < n;) {
-        cout << i << ':';
-        while (nxt < n && a[nxt] >= a[i]) nxt ++;
-        LL x = p[nxt] - p[i];
-        cout << x << '\n';
-        sum += (x + d - 1) / d;
-        ans += (sum - x - d + 1) / d * a[i];
-        i = nxt;
+    LL mi = 1e15, pre = 0, ans = 0;
+    for (int i = 1;i < n;i ++) {
+        pre += v[i];
+        mi = min(mi, a[i]);
+        if (pre > 0) {
+            ans += (pre + d - 1) / d * mi; 
+            pre -= (pre + d - 1) / d * d;
+        }
     }
     cout << ans << '\n';
 }
