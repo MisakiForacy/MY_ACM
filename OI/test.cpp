35abf47da991f7114ef73cbb9ea8a67e1dc8b387
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-22 14:00:55 +0800

    Auto sync 2026-07-22 14:00:55

--- OI/test.cpp
+++ OI/test.cpp
@@ -1,5 +1,10 @@
 #include <bits/stdc++.h>
 
+#define lowbit(x) (x & (-x))
+
+#define all(x) begin(x),end(x)
+#define siz(x) ((int)x.size())
+
 using namespace std;
 
 using LL = long long;
@@ -8,35 +13,39 @@ const int dx[] = {0, 1, 0, -1};
 const int dy[] = {1, 0, -1, 0};
 
 void solve() {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<int>> a(n + 2, vector<int> (m + 2, 0));
-    for (int i = 1;i <= n;i ++) {
-        for (int j = 1;j <= m;j ++) {
-            char c; cin >> c;
-            a[i][j] = c - '0';
-        }
-    }
-    vector<vector<int>> vis(n + 2, vector<int> (m + 2, 0));
-    int cnt = 0;
-    auto dfs = [&](auto dfs, int x, int y) -> void {
-        vis[x][y] = 1;
-        for (int k = 0;k < 4;k ++) {
-            int nx = x + dx[k], ny = y + dy[k];
-            if (!vis[nx][ny] && a[nx][ny]) {
-                dfs(dfs, nx, ny);
-            }
-        }
+    int n;
+    cin >> n;
+    vector<int> a(n + 1), fa(n + 1, 0), alls;
+    vector<vector<int>> g(n + 1);
+    for (int i = 1;i <= n;i ++) cin >> a[i], alls.push_back(a[i]);
+    for (int i = 2;i <= n;i ++) 
+        cin >> fa[i], g[fa[i]].push_back(i);
+    vector<int> f(n + 1, 0), ans(n + 1, 0);
+    
+    sort(all(alls));
+
+    for (int i = 1;i <= n;i ++)
+        a[i] = lower_bound(all(alls), a[i]) - alls.begin() + 1;
+
+    auto add = [&](int x, int val) -> void {
+        for (;x <= n;x += lowbit(x)) f[x] += val;
     };
-    for (int i = 1;i <= n;i ++) {
-        for (int j = 1;j <= m;j ++) {
-            if (a[i][j] && !vis[i][j]) {
-                dfs(dfs, i, j);
-                cnt ++;
-            }
+
+    auto qry = [&](int x) -> int {
+        int sum = 0;
+        for (;x;x -= lowbit(x)) sum += f[x];
+        return sum;
+    };
+
+    auto dfs = [&](auto dfs, int u) -> void {
+        for (int v : g[u]) {
+            dfs(dfs, v);
         }
-    }
-    cout << cnt << '\n';
+        ans[u] = qry(n) - qry(a[u]);
+        add(a[u]);
+    };
+    dfs(dfs, 1);
+    for (int i = 1;i <= n;i ++) cout << ans[i] << '\n';
 }
 
 int main() {
