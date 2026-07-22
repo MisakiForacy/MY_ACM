f91a49ebac77502c522807447ed55b50e2729fb9
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-22 09:01:03 +0800

    Auto sync 2026-07-22 09:01:03

--- HDOJ/2026-7-21/test.cpp
+++ HDOJ/2026-7-21/test.cpp
@@ -4,82 +4,47 @@ using namespace std;
 
 using LL = long long;
 
+const int N = 30;
+
+const int off = N;
+
+int ans = 0;
 int n;
-int a[1205][1205];
+int a[N], px[2 * N], qx[2 * N], h[N];
 
-const LL inf = 1e15;
-
-const int dx1[] = {0, 1, 0, -1};
-const int dy1[] = {1, 0, -1, 0};
-
-const int dx2[] = {1, 1, -1, -1};
-const int dy2[] = {1, -1, 1, -1};
-
-struct node
-{
-    LL x, y, w;
-    bool operator < (const node&T) const {
-        return w < T.w;
+void dfs(int pos) {
+    if (pos > n) {
+        if (!ans) {
+            for (int i = 1;i <= n;i ++) {
+                cout << a[i] << ' ';
+            }
+            cout << '\n';
+        }
+        ans ++;
     }
-};
-
-LL bfs(int sx, int sy, int fx, int fy) {
-    if (a[sx][sy] == -1) return inf;
-    priority_queue<node> q;
-    int vis[n + 2][n + 2];
-    LL dis[n + 2][n + 2];
-    memset(vis, 0, sizeof vis);
-    for (int i = 0;i <= n + 1;i ++) {
-        for (int j = 0;j <= n + 1;j ++) {
-            dis[i][j] = inf;
+    for (int i = 1;i <= n;i ++) {
+        if (!h[i] && !px[pos - i + off] && !qx[pos + i]) {
+            h[i] = 1;
+            px[pos - i + off] = 1;
+            qx[pos + i] = 1;
+            a[pos] = i;
+            dfs(pos + 1);
+            h[i] = 0;
+            px[pos - i + off] = 0;
+            qx[pos + i] = 0;
         }
     }
-    q.push({sx, sy});
-    vis[sx][sy] = 1;
-    dis[sx][sy] = 1;
-    while (q.size()) {
-        node top = q.top();
-        int x = top.x, y = top.y, w = top.w;
-        q.pop();
-        for (int k = 0;k < 4;k ++) {
-            int nx = x + (a[x][y] <= 2 ? dx1[k] : dx2[k]);
-            int ny = y + (a[x][y] <= 2 ? dy1[k] : dy2[k]);
-            if (nx < 1 || nx > n || ny < 1 || ny > n) continue;
-            if (vis[nx][ny] || a[nx][ny] == -1) continue;
-            vis[nx][ny] = 1;
-            dis[nx][ny] = min(dis[nx][ny], dis[x][y] + (2 - a[x][y] % 2));
-            q.push({nx, ny, dis[nx][ny]});
-        }
-    }
-    // cout << dis[fx][fy] << '\n';
-    return dis[fx][fy];
+}
+
+void solve() {
+    cin >> n;
+    dfs(1);
+    cout << ans << '\n';
 }
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0);
-    cin >> n;
-    for (int i = 1;i <= n;i ++) {
-        for (int j = 1;j <= n;j ++) {
-            char c;
-            cin >> c;
-            if (c == 'A') {
-                a[i][j] = 1;
-            } else if (c == 'B') {
-                a[i][j] = 2;
-            } else if (c == 'C') {
-                a[i][j] = 3;
-            } else {
-                a[i][j] = -1;
-            }
-        }
-    }
-    LL ans = inf;
-    ans = min(ans, bfs(1, 1, n, n));
-    ans = min(ans, bfs(1, n, n, n));
-    ans = min(ans, bfs(n, 1, n, n));
-    if (ans == inf) {
-        cout << "No answer\n";
-    } else {
-        cout << ans << '\n';
-    }
+    int T = 1;
+    // cin >> T;
+    while (T --) solve();
 }
