2babffa2fccb91096f3165b24eaa37ad13e478af
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-22 09:31:09 +0800

    Auto sync 2026-07-22 09:31:09

--- HDOJ/2026-7-21/test.cpp
+++ HDOJ/2026-7-21/test.cpp
@@ -1,45 +1,46 @@
 #include <bits/stdc++.h>
 
-using namespace std;
+#define x first 
+#define y second
 
+using namespace std;
 using LL = long long;
 
-const int N = 30;
+const int dx[] = {-2, -2, -1, 1, 2, 2, 1, -1};
+const int dy[] = {-1, 1, 2, 2, 1, -1, -2, -2};
 
-const int off = N;
+const int N = 405;
 
-int ans = 0;
-int n;
-int a[N], px[2 * N], qx[2 * N], h[N];
-
-void dfs(int pos) {
-    if (pos > n) {
-        if (!ans) {
-            for (int i = 1;i <= n;i ++) {
-                cout << a[i] << ' ';
-            }
-            cout << '\n';
-        }
-        ans ++;
-    }
-    for (int i = 1;i <= n;i ++) {
-        if (!h[i] && !px[pos - i + off] && !qx[pos + i]) {
-            h[i] = 1;
-            px[pos - i + off] = 1;
-            qx[pos + i] = 1;
-            a[pos] = i;
-            dfs(pos + 1);
-            h[i] = 0;
-            px[pos - i + off] = 0;
-            qx[pos + i] = 0;
-        }
-    }
-}
+int a[N][N], vis[N][N];
 
 void solve() {
-    cin >> n;
-    dfs(1);
-    cout << ans << '\n';
+    int n, m, x, y;
+    cin >> n >> m >> x >> y;
+    memset(a, -1, sizeof(a));
+    queue<pair<int, int>> q;
+    q.push({x, y});
+    a[x][y] = 0;
+    vis[x][y] = 1;
+    while (q.size()) {
+        pair<int, int> top = q.front();
+        int x = top.first;
+        int y = top.second;
+        q.pop();
+        for (int k = 0;k < 8;k ++) {
+            int nx = x + dx[k], ny = y + dy[k];
+            if ((1 <= nx && nx <= n && 1 <= ny && ny <= m) && !vis[nx][ny]) {
+                vis[nx][ny] = 1;
+                a[nx][ny] = a[x][y] + 1;
+                q.push({nx, ny});
+            }
+        }
+    }
+    for (int i = 1;i <= n;i ++) {
+        for (int j = 1;j <= m;j ++) {
+            cout << a[i][j] << ' ';
+        }
+        cout << '\n';
+    }
 }
 
 int main() {
