3ffae4679bd18d48614f28bf2fb2403ee772ede1
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-23 09:31:10 +0800

    Auto sync 2026-07-23 09:31:10

--- week/260722/H.cpp
+++ week/260722/H.cpp
@@ -10,35 +10,22 @@ using namespace std;
 using LL = long long;
 
 void solve() {
-    int n, k;
-    cin >> n >> k;
-    vector<int> a(n + 1);
+    int n, ans = 0;
+    cin >> n;
+    vector<int> a(n + 1), f(n + 1, 1);
     for (int i = 1;i <= n;i ++) cin >> a[i];
-    vector<vector<int>> g(n + 1);
-    for (int i = 1;i < n;i ++) {
-        int u, v;
-        cin >> u >> v;
-        g[u].push_back(v);
-        g[v].push_back(u);
+    for (int i = 1;i <= n;i ++) {
+        for (int j = 1;j < i;j ++) {
+            if (a[j] + 1 == a[i]) {
+                f[i] = max(f[i], f[j] + 1);
+                ans = max(ans, f[i]);
+            }
+        }
     }
-    int ans = 0;
-    auto dfs = [&](auto dfs, int u, int pu, int cnt, int K) -> void {
-        if (K < 0) {
-            ans = max(ans, cnt - 1);
-            return;
-        }
-        for (auto v : g[u]) {
-            if (v == pu) continue;
-            dfs(dfs, v, u, cnt + 1, K - (a[u] == 1));
-        }
-    };
-    dfs(dfs, 1, 0, 0, k);
     cout << ans << '\n';
 }
 
 int main() {
-    // freopen("move.in", "r", stdin);
-    // freopen("move.out", "w", stdout);
     ios::sync_with_stdio(0), cin.tie(0);
     int T = 1;
     // cin >> T;
