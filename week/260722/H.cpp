f259ac33f8100b0205354155fb8f017965ab56eb
MisakiForacy <fuxuanyu243@gmail.com>
2026-07-23 09:01:42 +0800

    Auto sync 2026-07-23 09:01:41

--- week/260722/H.cpp
+++ week/260722/H.cpp
@@ -1,45 +1,46 @@
 #include <bits/stdc++.h>
 
-using namespace std;
+#define x first 
+#define y second
 
+#define all(x) begin(x),end(x)
+#define siz(x) ((int)x.size())
+
+using namespace std;
 using LL = long long;
 
 void solve() {
-    int n;
-    cin >> n;
-    // LL s[26], t[26];
-    // memset(s, 0, sizeof s);
-    // memset(t, 0, sizeof t);
-    vector<LL> s(26, 0), t(26, 0);
-    s[0] = 1, t[0] = 1;
-    for (int i = 1;i <= n;i ++) {
-        LL d, k;
-        cin >> d >> k;
-        string ss;
-        cin >> ss;
-        for (char c : ss)
-            if (d == 1)
-                s[c - 'a'] += k;
-            else
-                t[c - 'a'] += k;
-        int ok = 0;
-        for (int j = 1;j <= 25;j ++) {
-            if (!t[j]) continue;
-            for (int k = 0;k < j;k ++) {
-                if (s[k]) ok = 1;
-            }
-        }
-        for (int j = 25;j >= 0;j --) {
-            if (s[j] < t[j]) {ok = 1; break;}
-            if (s[j] > t[j]) {break;}
-        }
-        cout << (ok ? "YES\n" : "NO\n");
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n + 1);
+    for (int i = 1;i <= n;i ++) cin >> a[i];
+    vector<vector<int>> g(n + 1);
+    for (int i = 1;i < n;i ++) {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
     }
+    int ans = 0;
+    auto dfs = [&](auto dfs, int u, int pu, int cnt, int K) -> void {
+        if (K < 0) {
+            ans = max(ans, cnt - 1);
+            return;
+        }
+        for (auto v : g[u]) {
+            if (v == pu) continue;
+            dfs(dfs, v, u, cnt + 1, K - (a[u] == 1));
+        }
+    };
+    dfs(dfs, 1, 0, 0, k);
+    cout << ans << '\n';
 }
 
-int main () {
+int main() {
+    // freopen("move.in", "r", stdin);
+    // freopen("move.out", "w", stdout);
     ios::sync_with_stdio(0), cin.tie(0);
     int T = 1;
-    cin >> T;
+    // cin >> T;
     while (T --) solve();
 }
