3c9f594143562eeb52cf9369c30b1ebb2a1fa2de
MisakiForacy <fuxuanyu243@gmail.com>
2026-08-07 14:01:00 +0800

    Auto sync 2026-08-07 14:01:00

---
+++ nowcoder/20260807/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define siz(x) ((int)x.size())
+
+#define all(x) begin(x),end(x)
+
+using namespace std;
+
+using LL = long long;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n + 1), p(32, 0);
+    for (int i = 1;i <= n;i ++) {
+        cin >> a[i];
+    }
+    for (int i = 1;i <= n;i ++) {
+        
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T --) solve();
+}
