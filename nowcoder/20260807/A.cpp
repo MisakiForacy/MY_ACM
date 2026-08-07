f39f6c54077b5bbb8c3149ca05b49a0b447ff4d5
MisakiForacy <fuxuanyu243@gmail.com>
2026-08-07 14:30:05 +0800

    Auto sync 2026-08-07 14:30:04

--- nowcoder/20260807/A.cpp
+++ nowcoder/20260807/A.cpp
@@ -15,9 +15,27 @@ void solve() {
     for (int i = 1;i <= n;i ++) {
         cin >> a[i];
     }
-    for (int i = 1;i <= n;i ++) {
-        
+    int f[n + 1][2];
+    memset(f, 0, sizeof(f));
+/*
+0001
+0000
+0001
+0110
+0101
+*/
+
+
+    // f[i][1] 表示考虑到第j位时f[i]和f[i-1]高位都相同
+    // f[i][0] 表示考虑到第j位时f[i]和f[i-1]高位不同
+    for (int i = 1;i <= n;i ++) f[i][1] = 1;
+    LL ans = 0;
+    for (int j = 30;j >= 0;j --) {
+        for (int i = 2;i <= n;i ++) {
+            
+        }
     }
+    cout << ans << '\n';
 }
 
 int main() {
