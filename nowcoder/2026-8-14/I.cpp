9d0e5ec7586c234c6ab57e7fe20e3c9b768860ea
MisakiForacy <fuxuanyu243@gmail.com>
2026-08-14 15:58:22 +0800

    Auto sync 2026-08-14 15:58:21

--- nowcoder/2026-8-14/I.cpp
+++ nowcoder/2026-8-14/I.cpp
@@ -7,44 +7,42 @@ using LL = long long;
 void solve() {
     LL x, a, b;
     cin >> x >> a >> b;
-    // LL lo = 0, hi = 1e9;
-    // LL A = (a + 1) * a / 2 + (a - a);
-    // LL B = b * a;
-    auto case1 = [&]() -> LL {
-        LL d = a - 1;
-        LL s = a;
-        LL lo = 1, hi = x; 
-        auto check = [&](LL x) -> bool {
-            return s + (x - 1) * d < x;
+    LL lo = 1, hi = x;
+    LL s = a, d = a - 1;
+    auto check = [&](LL mid) -> bool {
+        auto cal = [&](LL len) -> LL {
+            LL f = (len ? s + (len - 1) * d : 0);
+            LL fs = max(0LL, f - 1);
+            LL ff = max(0LL, fs - (mid - len) + 1);
+            LL ct = fs - ff + 1;
+            LL a1 = (s + f) * len / 2;
+            LL a2 = (mid - len) * b + ct * (fs + ff) / 2;
+            return a1 + a2;
         };
-        while (lo < hi) {
-            LL mid = lo + hi >> 1;
-            if (check(mid))
-                lo = mid + 1;
+        LL l = 0, r = mid;
+        while (l < r) {
+            LL m = l + r >> 1;
+            if (cal(m) < cal(m + 1))
+                l = m + 1;
             else 
-                hi = mid;
+                r = m;
         }
-        return lo;
+        cout << mid << ' ' << l << l << ' ' << cal(l) << '\n';
+        return cal(l) < x;
     };
-    auto case2 = [&]() -> LL {
-        return (x + b - 1) / b;
-    };
-    auto case3 = [&]() -> LL {
-        
-    };
-    // while (lo < hi) {
-    //     LL mid = lo + hi >> 1;
-    //     if (check(mid))
-    //         lo = mid + 1;
-    //     else 
-    //         hi = mid;
-    // }
-    // cout << lo << '\n';
+    while (lo < hi) {
+        LL mid = lo + hi >> 1;
+        if (check(mid))
+            lo = mid + 1;
+        else 
+            hi = mid;
+    }
+    cout << lo << '\n';
 }
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0);
     int T = 1;
-    cin >> T;
+    // cin >> T;
     while (T --) solve();
 }
