7a77257a9eec69f051f050a124f5fa84e7757245
MisakiForacy <fuxuanyu243@gmail.com>
2026-08-19 15:58:08 +0800

    Auto sync 2026-08-19 15:58:08

--- nowcoder/2026-8-19/L.cpp
+++ nowcoder/2026-8-19/L.cpp
@@ -19,25 +19,35 @@ const int N = 24;
 const LL inf = 1e15;
 
 void solve() {
-    cout << fixed << setprecision(10);
+    cout << fixed << setprecision(16);
     LL m, r, c;
     cin >> m >> r >> c;
-    if (m + r <= c) {
+    if (m + r < c) {
         LL k = 1;
         while (k * (m + r) < c) {
             k ++;
         }
         if (k * (m + r) - r >= c) {
             LL p = k * m;
-            LL t = (k - 1) * (m + r) + m;
+            LL t = k * (m + r) - r + m;
             cout << 1.0 * p / t << '\n';
         } else {
             LL p = (k + 1) * m;
             LL t = k * (m + r) + m;
-            cout << 1.0 * p / t << '\n';
+            LL v = c - (k * (m + r) - r);
+            long double ans1 = 1.0 * p / t;
+            t = v + k * (m + r) - r + m;
+            long double ans2 = 1.0 * p / t;
+            cout << max(ans1, ans2) << '\n';
         }
-    } else {
-        cout << 1.0 * m / (1.0 * max(m, c)) << '\n';
+    } else if (c > m) {
+        long double ans1 = 1.0 * m / c;
+        LL p = 2 * m;
+        LL t = 2 * m + r;
+        long double ans2 = 1.0 * p / t;
+        cout << max(ans1, ans2) << '\n';
+    } else if (c < m) {
+        cout << 1.0 << '\n';
     }
 }
 
