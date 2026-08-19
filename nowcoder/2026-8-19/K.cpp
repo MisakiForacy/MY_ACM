2c1d14aa69baf8e9d1c408d433563bfd09c20127
MisakiForacy <fuxuanyu243@gmail.com>
2026-08-19 13:28:35 +0800

    Auto sync 2026-08-19 13:28:34

--- nowcoder/2026-8-19/K.cpp
+++ nowcoder/2026-8-19/K.cpp
@@ -14,29 +14,60 @@ using LL = long long;
 
 using PLL = pair<LL, LL>;
 
-const int N = 25;
+const int N = 24;
+
+const LL inf = 1e15;
 
 int n;
-LL f[N][N], use[N];
+LL f[N][N], dp[1 << N], use[N];
 
-void dfs(int pos) {
-    if (pos == n * 3) {
-        LL res = 0;
-        for (int i = 2;i <= 3 * n) {
-            res += f[use[i - 1]][use[i]];
-        }
-        
-    }
-}
+map<int, set<int>> mp;
 
 void solve() {
     cin >> n;
-    for (int i = 1;i <= 3 * n;i ++) {
-        for (int j = 1;j <= 3 * n;j ++) {
+    for (int i = 0;i < 3 * n;i ++) {
+        for (int j = 0;j < 3 * n;j ++) {
             cin >> f[i][j];
         }
     }
+    for (int i = 0;i < 1LL << N - 1;i ++) dp[i] = - inf;
+    for (int i = 0;i < 3 * n;i ++) {
+        for (int j = i + 1;j < 3 * n;j ++) {
+            for (int k = j + 1;k < 3 * n;k ++) {
+                LL base = (1 << i) + (1 << j) + (1 << k);
+                dp[base] = f[i][j] + f[i][k] + f[j][k];
+                mp[3].insert(base);
+            }
+        }
+    }
+    for (int t = 3;t < 3 * n;t += 3) {
+        for (auto base : mp[t]) {
+            for (int i = 0;i < 3 * n;i ++) {
+                if (base >> i & 1) continue;
+                for (int j = i + 1;j < 3 * n;j ++) {
+                    if (base >> j & 1) continue;
+                    for (int k = j + 1;k < 3 * n;k ++) {
+                        if (base >> k & 1) continue;
+                        LL mask = ((1 << i) + (1 << j) + (1 << k));
+                        dp[mask ^ base] = max(dp[mask ^ base], dp[base] + dp[mask]);
+                        mp[t + 3].insert(mask ^ base);
+                    }
+                }
+            }
+        }
+    }
+    cout << dp[(1 << 3 * n) - 1] << '\n';
 }
+
+/*
+2
+0 5 4 -1 -2 -3
+5 0 3 -2 -2 -3
+4 3 0 -1 -4 -2
+-1 -2 -1 0 7 6
+-2 -2 -4 7 0 5
+-3 -3 -2 6 5 0
+*/
 
 int main() {
     // int cnt = 0;
@@ -59,8 +90,8 @@ int main() {
     // cout << '\n';
     // cout << tot << '\n';
     // cout << cnt << '\n';
-    // ios::sync_with_stdio(0), cin.tie(0);
-    // int T = 1;
-    // // cin >> T;
-    // while (T --) solve();
+    ios::sync_with_stdio(0), cin.tie(0);
+    int T = 1;
+    // cin >> T;
+    while (T --) solve();
 }
