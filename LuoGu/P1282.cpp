2a06e6527562f4d4a9d0d1ff95c59e7942af88b7
MisakiForacy <fuxuanyu243@gmail.com>
2026-08-28 21:29:53 +0800

    Auto sync 2026-08-28 21:29:52

--- LuoGu/P1282.cpp
+++ LuoGu/P1282.cpp
@@ -12,11 +12,17 @@ int main() {
     int n;
     cin >> n;
     for (int i = 0;i < N;i ++) dp[i][0] = dp[i][1] = 1e8;
-    dp[0] = 0, ;
+    dp[0][0] = dp[0][1] = 0;
     for (int i = 1;i <= n;i ++) {
         int a, b, c;
         cin >> a >> b;
         c = a - b;
-        dp[i] = dp[i - 1]
+        dp[i][0] = dp[i - 1][0] + c;
+        if (abs(dp[i - 1][1] + c) < abs(dp[i][0])) 
+            dp[i][0] = dp[i - 1][1] + c;
+        dp[i][1] = dp[i - 1][0] - c;
+        if (abs(dp[i - 1][1] - c) < abs(dp[i][1]))
+            dp[i][1] = dp[i - 1][1] - c;
     }
+    if 
 }
