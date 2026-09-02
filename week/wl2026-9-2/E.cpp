a02fd2a4367acfabfc02388f2d9be673208447c1
MisakiForacy <fuxuanyu243@gmail.com>
2026-09-02 18:39:59 +0800

    Auto sync 2026-09-02 18:39:59

---
+++ week/wl2026-9-2/E.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define x first 
+
+#define y second 
+
+#define siz(x) ((int)x.size())
+
+#define all(x) begin(x),end(x)
+
+using namespace std;
+
+using LL = long long;
+
+using PLL = pair<LL, LL>;
+
+LL qmi(LL x, LL k) {
+    LL res = 1;
+    while (k) {
+        if (k & 1) res *= x;
+        x = x * x;
+        k >>= 1;
+    }
+    return res;
+}
+
+void solve() {
+    LL a, b, c;
+    cin >> a >> b >> c;
+    vector<LL> ans;   
+    for (int s = 1;s <= 100;s ++) {
+        LL x = b * qmi(s, a) + c;
+        if (x > 0 && x < 1e9) ans.push_back(x);
+    }
+    sort(all(ans));
+    cout << siz(ans) << '\n';
+    for (LL x : ans) cout << x << ' ';
+    cout << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int T = 1;
+    // cin >> T;
+    while (T --) solve();
+}
