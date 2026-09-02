141f5fa48765f7c83a518f4f3220ac8f73872f84
MisakiForacy <fuxuanyu243@gmail.com>
2026-09-02 19:39:52 +0800

    Auto sync 2026-09-02 19:39:52

--- week/wl2026-9-2/D.cpp
+++ week/wl2026-9-2/D.cpp
@@ -22,8 +22,15 @@ void solve() {
     int a[k + 1];
     memset(a, 0, sizeof a);
     for (int i = 1;i <= n;i ++) {
-        a[(i - 1) % k + 1] ++'' 
+        int x;
+        cin >> x;
+        a[(i - 1) % k + 1] += (x == 1); 
     }
+    LL ans = 0;
+    for (int i = 1;i <= k;i ++) {
+        ans += min(n / k - a[i], a[i]);
+    }
+    cout << ans << '\n';
 }
 
 int main() {
