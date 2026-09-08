2d9f42fa59a06796176f5ed981784cb26cbaa392
MisakiForacy <fuxuanyu243@gmail.com>
2026-09-08 14:39:00 +0800

    Auto sync 2026-09-08 14:39:00

---
+++ AKICPC/The 2026 ICPC Asia East Continent Online Contest (I)/C.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using LL = long long;
+
+struct node
+{
+    
+};
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<LL> a(n + 1);
+    for (int i = 1;i <= k;i ++) {
+        int l, r;
+        cin >> l >> r;
+        vector<node> p;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T --) solve();
+}
