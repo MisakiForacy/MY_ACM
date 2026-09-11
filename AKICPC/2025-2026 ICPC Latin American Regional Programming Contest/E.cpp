f71b53e39ee9cfe2fa59dc58e348901d2c2217d2
MisakiForacy <fuxuanyu243@gmail.com>
2026-09-11 12:21:08 +0800

    Auto sync 2026-09-11 12:21:08

--- AKICPC/2025-2026 ICPC Latin American Regional Programming Contest/E.cpp
+++ AKICPC/2025-2026 ICPC Latin American Regional Programming Contest/E.cpp
@@ -8,6 +8,8 @@
 
 #define all(x) begin(x),end(x)
 
+#define lowbit(x) ((x) & (-x))
+
 using namespace std;
 
 using LL = long long;
@@ -17,6 +19,7 @@ void solve() {
     cin >> n;
     vector<LL> f(n + 1, 0), p(n + 1), use(n + 1), alls;
     vector<char> op(n + 1);
+    map<LL, LL> val;
     for (int i = 1;i <= n;i ++) {
         cin >> op[i] >> p[i];
         alls.push_back(p[i]);
@@ -25,8 +28,38 @@ void solve() {
     alls.erase(unique(all(alls)), alls.end());
     for (int i = 1;i <= n;i ++) {
         use[i] = lower_bound(all(alls), p[i]) - p.begin() + 1;
+        val[use[i]] = p[i];
     }
-    auto ask = [&]()
+    auto upd = [&](int x, int v) -> void {
+        for (;x <= n;x += lowbit(x)) f[x] += v;
+    };
+    auto ask = [&](int x) -> LL {
+        LL res = 0;
+        for (;x;x -= lowbit(x)) res += f[x];
+        return res;
+    };
+    auto getKth = [&](int k) {
+        int l = 1, r = n;
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (ask(mid) >= k)
+                r = mid;
+            else   
+                l = mid + 1;
+        }
+        return l;
+    };
+    int tot = 0;
+    for (int i = 1;i <= n;i ++) {
+        if (op[i] == '+') {
+            upd(use[i], 1);
+            tot ++;
+        } else {
+            upd(use[i], -1);
+            tot --;
+        }
+        
+    }
 }
 
 int main() {
