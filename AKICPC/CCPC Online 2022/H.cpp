720aeb69d100a760c3340530870f431e81fd075c
MisakiForacy <fuxuanyu243@gmail.com>
2026-09-17 21:31:11 +0800

    Auto sync 2026-09-17 21:31:11

---
+++ AKICPC/CCPC Online 2022/H.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using LL = long long;
+
+void solve() {
+    LL L, R, K;
+    cin >> L >> R >> K;
+    for (LL n = 0;n <= 60;n ++) {
+        if (1LL << n > )
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T --) solve();
+}
